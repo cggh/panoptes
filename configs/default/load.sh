ab60615a1673c8e0c822353dbe37b87906f84575
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2013-11-15 15:32:24 +0000

    Simple test data

--- configs/default/load.sh
+++ configs/default/load.sh
@@ -1,5 +1,3 @@
 #!/bin/bash -e
-mysql -h$DBHOST -u$DBUSER -p$DBPASS -e "DROP DATABASE IF EXISTS datasetindex;CREATE DATABASE datasetindex;"
-mysql -h$DBHOST -u$DBUSER -p$DBPASS -Ddatasetindex < default.sql
-mysql -h$DBHOST -u$DBUSER -p$DBPASS -e "DROP DATABASE IF EXISTS pf21viewtracks;CREATE DATABASE pf21viewtracks;"
-mysql -h$DBHOST -u$DBUSER -p$DBPASS -Dpf21viewtracks < default2.sql
+mysql -h$DBHOST -u$DBUSER -p$DBPASS < default.sql
+
