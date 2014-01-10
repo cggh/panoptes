51e0926ee880aafc00cc2b6de90faec53888b7c4
Paul Vauterin <paul.vauterin@gmail.com>
2014-01-10 17:01:51 +0000

    Show map points selection

--- webapp/scripts/Plots/GeoMapPoints.js
+++ webapp/scripts/Plots/GeoMapPoints.js
@@ -1,7 +1,7 @@
-define(["require", "DQX/base64", "DQX/Application", "DQX/DataDecoders", "DQX/Framework", "DQX/Controls", "DQX/Msg", "DQX/SQL", "DQX/DocEl", "DQX/Utils", "DQX/Wizard", "DQX/Popup", "DQX/PopupFrame", "DQX/Map", "DQX/DataFetcher/DataFetchers", "Wizards/EditQuery",
+define(["require", "DQX/base64", "DQX/Application", "DQX/DataDecoders", "DQX/Framework", "DQX/Controls", "DQX/Msg", "DQX/SQL", "DQX/DocEl", "DQX/Utils", "DQX/Wizard", "DQX/Popup", "DQX/PopupFrame", "DQX/Map", "DQX/DataFetcher/DataFetchers", "Wizards/EditQuery", "DQX/GMaps/PointSet",
     "MetaData",
     "Utils/QueryTool", "Plots/GenericPlot"],
-    function (require, base64, Application, DataDecoders, Framework, Controls, Msg, SQL, DocEl, DQX, Wizard, Popup, PopupFrame, Map, DataFetchers, EditQuery,
+    function (require, base64, Application, DataDecoders, Framework, Controls, Msg, SQL, DocEl, DQX, Wizard, Popup, PopupFrame, Map, DataFetchers, EditQuery, PointSet,
               MetaData,
               QueryTool, GenericPlot) {
 
@@ -29,7 +29,7 @@ define(["require", "DQX/base64", "DQX/Application", "DQX/DataDecoders", "DQX/Fra
             var eventid = DQX.getNextUniqueID();that.eventids.push(eventid);
             Msg.listen(eventid,{ type: 'SelectionUpdated'}, function(scope,tableid) {
                 if (that.tableInfo.id==tableid)
-                    that.reDraw();
+                    that.updateSelection();
             } );
 
 
@@ -68,6 +68,7 @@ define(["require", "DQX/base64", "DQX/Application", "DQX/DataDecoders", "DQX/Fra
                     cmdLassoSelection.changeContent('Lasso select Points');
                     that.theMap.stopLassoSelection();
                     cmdLassoSelection.busy = false;
+                    that.fetchLassoSelection();
                 };
 
                 var cmdLassoSelection = Controls.Button(null, { content: 'Lasso select Points'}).setOnChanged(function () {
@@ -98,7 +99,8 @@ define(["require", "DQX/base64", "DQX/Application", "DQX/DataDecoders", "DQX/Fra
                 that.panelButtons.addControl(controlsGroup);
 
                 that.theMap = Map.GMap(this.framePlot);
-                that.pointSet = Map.PointSet('points', that.theMap, 0, "", { showLabels: false, showMarkers: true });
+                //that.pointSet = Map.PointSet('points', that.theMap, 0, "", { showLabels: false, showMarkers: true });
+                that.pointSet = PointSet.Create(that.theMap, {});
                 that.pointSet.setPointClickCallBack(function(itemid) {
                     Msg.send({ type: 'ItemPopup' }, { tableid: that.tableInfo.id, itemid: itemid } );
                 });
@@ -123,21 +125,19 @@ define(["require", "DQX/base64", "DQX/Application", "DQX/DataDecoders", "DQX/Fra
                 fetcher.addColumn(that.tableInfo.primkey, 'ST');
                 fetcher.addColumn(that.tableInfo.propIdGeoCoordLongit, 'F3');
                 fetcher.addColumn(that.tableInfo.propIdGeoCoordLattit, 'F3');
+                that.points_Keys = null;
+                that.points_Longitude = null;
+                that.points_Lattitude = null;
                 that.pointSet.clearPoints();
+                that.points = null;
                 var requestID = DQX.getNextUniqueID();
+                var selectionInfo = that.tableInfo.currentSelection;
                 fetcher.getData(that.theQuery.get(), that.tableInfo.primkey,
                     function (data) { //success
-                        var points = [];
-                        for (var nr =0; nr<data.Longitude.length; nr++) {
-                            var pt =
-                            {
-                                id: data[that.tableInfo.primkey][nr],
-                                longit: data.Longitude[nr],
-                                lattit: data.Lattitude[nr]
-                            }
-                            points.push(pt);
-                        }
-                        that.pointSet.setPoints(points);
+                        that.points_Keys = data[that.tableInfo.primkey];
+                        that.points_Longitude = data.Longitude;
+                        that.points_Lattitude = data.Lattitude;
+                        that.setPoints();
                         if (that.startZoomFit) {
                             that.pointSet.zoomFit(100);
                             that.startZoomFit = false;
@@ -151,6 +151,54 @@ define(["require", "DQX/base64", "DQX/Application", "DQX/DataDecoders", "DQX/Fra
                 );
             }
 
+            that.setPoints = function() {
+                var keys = that.points_Keys;
+                var longitudes = that.points_Longitude;
+                var lattitudes = that.points_Lattitude;
+                var selectionInfo = that.tableInfo.currentSelection;
+                that.points = [];
+                for (var nr =0; nr<keys.length; nr++) {
+                    var itemid = keys[nr];
+                    var pt =
+                    {
+                        id: itemid,
+                        longit: longitudes[nr],
+                        lattit: lattitudes[nr],
+                        sel: !!(selectionInfo[itemid])
+                    }
+                    that.points.push(pt);
+                }
+                that.pointSet.setPoints(that.points);
+            }
+
+            that.updateSelection = function() {
+                if (!that.points)  return;
+                var points = that.points;
+                var selectionInfo = that.tableInfo.currentSelection;
+                for (var nr =0; nr<points.length; nr++) {
+                    var cursel = !!(selectionInfo[points[nr].id]);
+                    if (points[nr].sel != cursel) {
+                        points[nr].sel = cursel;
+                        that.pointSet.updateSelectionState(nr);
+                    }
+                }
+            }
+
+            that.fetchLassoSelection = function() {
+                if (!that.points)  return;
+                var points = that.points;
+                var selectionInfo = that.tableInfo.currentSelection;
+                var modified = false;
+                for (var nr =0; nr<points.length; nr++) {
+                    var sel = that.theMap.isCoordInsideLassoSelection(Map.Coord(points[nr].longit, points[nr].lattit));
+                    if (sel!=!!(selectionInfo[points[nr].id])) {
+                        modified = true;
+                        that.tableInfo.selectItem(points[nr].id, sel);
+                    }
+                }
+                if (modified)
+                    Msg.broadcast({type:'SelectionUpdated'}, that.tableInfo.id);
+            }
 
 
             that.reloadAll = function() {
