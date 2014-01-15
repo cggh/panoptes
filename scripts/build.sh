dadfee7d5dbdfd98652090801680e53eb8d3e4f3
Ben Jeffery <ben.jeffery@Well.ox.ac.uk>
2014-01-15 00:27:57 +0000

    Build skeleton DB as part of build. Build numpy outside of requirements

--- scripts/build.sh
+++ scripts/build.sh
@@ -51,6 +51,8 @@ cd DQXServer
 source bin/activate
 pip install -q -r REQUIREMENTS
 #Extra ones for custom responder
+#Have to do numpy first as h5py does not stipulate it as an install requirement....
+pip install -q numpy
 pip install -q -r $PROJECT_ROOT/servermodule/REQUIREMENTS
 pip install -q gunicorn #For testing, not a strict requirement of DQXServer
 
@@ -71,6 +73,21 @@ echo -e "${red}  Linking static content into DQXServer${NC}"
 cd $PROJECT_ROOT/build/dependencies/DQXServer
 ln -s $PROJECT_ROOT/webapp static
 
+echo -e "${red}  Creating skeleton DB - if needed${NC}"
+DBSRV=`python -c "import config;print config.DBSRV"`
+DBUSER=`python -c "import config;print config.DBUSER"`
+DBPASS=`python -c "import config;print config.DBPASS"`
+DB=`python -c "import config;print config.DB"`
+mysql -h$DBSRV -u$DBUSER -p$DBPASS <<- EOF
+CREATE DATABASE IF NOT EXISTS ${DB};
+USE ${DB};
+CREATE TABLE IF NOT EXISTS datasetindex  (
+   id  varchar(20) DEFAULT NULL,
+   name  varchar(50) DEFAULT NULL
+);
+EOF
+
+
 echo -e "${green}Done!${NC}"
 
 #if [ -z "$WSGI_FOLDER" ]; then
@@ -82,5 +99,11 @@ echo -e "${green}Done!${NC}"
 #	ln -s $PROJECT_ROOT/dependencies/DQXServer/* $WSGI_FOLDER/panoptes/$CONFIG/.
 #fi
 # sudo apt-get install gcc gfortran python-dev libblas-dev liblapack-dev cython
+# sudo apt-get install libmysqlclient-dev
+#wget https://github.com/n1k0/casperjs/zipball/1.1-beta3
+#unzip 1.1-beta3
+#wget https://phantomjs.googlecode.com/files/phantomjs-1.9.2-linux-x86_64.tar.bz2
+#tar xjvf phantomjs-1.9.2-linux-x86_64.tar.bz2
+
 popd  > /dev/null
 
