00c18dcca39e1ce5b79b8b38d53658e510077577
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2014-01-24 16:29:41 +0000

    Rework 2D query responder for imported format data

--- servermodule/2d_server/2d_query.py
+++ servermodule/2d_server/2d_query.py
@@ -1,122 +1,89 @@
 import DQXDbTools
-from file_dict import FileDict
-from struct import pack
-import StringIO
-from gzip import GzipFile
-from itertools import islice, takewhile
-from operator import ne
-from functools import partial
-import hashlib
+import os
 import h5py
+import itertools
 import numpy as np
 import config
 import arraybuffer
+from gzipstream import gzip
 
-#TODO cache doesn't have locking....
-cache = FileDict('cache')
+#curl 'http://localhost:8000/app01?datatype=custom&respmodule=2d_server&respid=2d_query&dataset=Genotypes&col_qry=eyJ3aGNDbGFzcyI6ImNvbXBvdW5kIiwiaXNDb21wb3VuZCI6dHJ1ZSwiVHBlIjoiQU5EIiwiQ29tcG9uZW50cyI6W3sid2hjQ2xhc3MiOiJjb21wb3VuZCIsImlzQ29tcG91bmQiOnRydWUsIlRwZSI6IkFORCIsIkNvbXBvbmVudHMiOlt7IndoY0NsYXNzIjoiY29tcGFyZWZpeGVkIiwiaXNDb21wb3VuZCI6ZmFsc2UsIkNvbE5hbWUiOiJwb3MiLCJUcGUiOiI8IiwiQ29tcFZhbHVlIjoiMTAwMDAwIn1dfSx7IndoY0NsYXNzIjoiY29tcGFyZWZpeGVkIiwiaXNDb21wb3VuZCI6ZmFsc2UsIkNvbE5hbWUiOiJjaHJvbSIsIlRwZSI6Ij0iLCJDb21wVmFsdWUiOiIyTCJ9LHsid2hjQ2xhc3MiOiJjb21wYXJlZml4ZWQiLCJpc0NvbXBvdW5kIjpmYWxzZSwiQ29sTmFtZSI6InBvcyIsIlRwZSI6Ij49IiwiQ29tcFZhbHVlIjo3ODQwNn0seyJ3aGNDbGFzcyI6ImNvbXBhcmVmaXhlZCIsImlzQ29tcG91bmQiOmZhbHNlLCJDb2xOYW1lIjoicG9zIiwiVHBlIjoiPCIsIkNvbXBWYWx1ZSI6OTk5OTh9XX0=&row_qry=eyJ3aGNDbGFzcyI6InRyaXZpYWwiLCJpc0NvbXBvdW5kIjpmYWxzZSwiVHBlIjoiIiwiaXNUcml2aWFsIjp0cnVlfQ==&datatable=genotypes&property=first_allele&col_order=genotypes_column_index&row_order=genotypes_row_index' -H 'Pragma: no-cache' -H 'Accept-Encoding: gzip,deflate,sdch' --compressed
 
-callset_fn = config.HDF5_FILE
-callset = h5py.File(callset_fn, 'r')
-allele_depth = callset['calldata_2d']['AD']
-
-import collections
-import functools
-
-def gzip(data):
-    out = StringIO.StringIO()
-    f = GzipFile(fileobj=out, mode='w')
-    f.write(data)
-    f.close()
-    return out.getvalue()
-
-def index_from_query(database, table, query, order=None):
-    db = DQXDbTools.OpenDatabase(database)
+def index_from_query(db, table, index_column, query, order):
     cur = db.cursor()
-
-    whc=DQXDbTools.WhereClause()
-    whc.ParameterPlaceHolder='%s'#NOTE!: MySQL PyODDBC seems to require this nonstardard coding
-    whc.Decode(query)
-    whc.CreateSelectStatement()
-
-    sqlquery = "SELECT "
-    sqlquery += "idx FROM {0}".format(table)
-    if len(whc.querystring_params)>0:
-        sqlquery += " WHERE {0}".format(whc.querystring_params)
-    if order:
-        sqlquery += " ORDER BY {0}".format(order)
-    print('################################################')
-    print('###QRY:'+sqlquery)
-    print('###PARAMS:'+str(whc.queryparams))
-    print('################################################')
-    cur.execute(sqlquery,whc.queryparams)
-    for row in cur.fetchall():
-        yield row[0]
+    where = DQXDbTools.WhereClause()
+    where.ParameterPlaceHolder = '%s'#NOTE!: MySQL PyODDBC seems to require this nonstardard coding
+    where.Decode(query)
+    where.CreateSelectStatement()
+    query = "WHERE " + where.querystring_params if len(where.querystring_params) > 0 else ''
+    sqlquery = "SELECT {index_column} FROM {table} {query} ORDER BY {order}".format(**locals())
+    cur.execute(sqlquery, where.queryparams)
+    result = [row[0] for row in cur.fetchall()]
     cur.close()
-    db.close()
+    return result
 
-def select_by_list(dataset, selection):
-    #TODO Selecting ~200 at a time is about 4x faster....
-    sel = h5py._hl.selections.PointSelection(dataset.shape)
-    sel.set(selection)
-    out = np.ndarray(sel.mshape, dataset.id.dtype)
-    dataset.id.read(h5py.h5s.create_simple(sel.mshape),
-                      sel._id,
-                      out,
-                      h5py.h5t.py_create(dataset.id.dtype))
-    return out
-        
+
+def select_by_list(property_array, col_idx, row_idx):
+    num_cells = len(col_idx) * len(row_idx)
+    coords = ((row, col) for row in row_idx for col in col_idx)
+    CHUNK_SIZE = 400
+    result_array = np.empty((num_cells,), dtype=property_array.id.dtype)
+    for i in xrange((num_cells / CHUNK_SIZE) + 1):
+        selection = np.asarray(list(itertools.islice(coords, CHUNK_SIZE)))
+        print selection
+        sel = h5py._hl.selections.PointSelection(property_array.shape)
+        sel.set(selection)
+        out = np.ndarray(sel.mshape, property_array.id.dtype)
+        property_array.id.read(h5py.h5s.create_simple(sel.mshape),
+                               sel._id,
+                               out,
+                               h5py.h5t.py_create(property_array.id.dtype))
+        result_array[i * CHUNK_SIZE: (i * CHUNK_SIZE)+len(selection)] = out[:]
+    result_array.reshape((len(row_idx), len(col_idx)))
+    return result_array
+
+
+def get_table_names(db, datatable):
+    cur = db.cursor()
+    sql = 'SELECT col_table, row_table FROM 2D_tablecatalog WHERE id=%s'
+    cur.execute(sql, (datatable,))
+    result = cur.fetchall()[0]
+    cur.close()
+    return result
+
 
 def response(request_data):
     return request_data
 
-def split(n, gen):
-    remaining = True
-    gen = iter(gen)
-    while remaining:
-        out = []
-        for x in xrange(n):
-            try:
-                out.append(gen.next())
-            except StopIteration:
-                remaining = False
-                break
-        if len(out) > 0:
-            yield out
-
-def read_count_stream(coord_groups):
-    for group in coord_groups:
-        depth = select_by_list(allele_depth, group)
-        for a, r in depth:
-            yield a
-            yield r
 
 def handler(start_response, request_data):
-    key = hashlib.sha224((request_data['database'] + 
-                          request_data['var_tbname'] +
-                          request_data['var_qry'] + 
-                          request_data['samp_tbname'] +
-                          request_data['samp_qry'] + 
-                          '_2d')).hexdigest()
-    try:
-        data = cache[key]
-    except KeyError:
-        var_idx = index_from_query(request_data['database'], 
-        	                           request_data['var_tbname'],
-                                       request_data['var_qry'],
-                                       'pos')
+    datatable = request_data['datatable']
+    dataset = request_data['dataset']
+    property = request_data['property']
+    col_qry = request_data['col_qry']
+    col_order = request_data['col_order']
+    row_qry = request_data['row_qry']
+    row_order = request_data['row_order']
 
-        samp_idx = index_from_query(request_data['database'],
-        	                           request_data['samp_tbname'],
-                                       request_data['samp_qry'],
-                                       'ox_code')
+    db = DQXDbTools.OpenDatabase(dataset)
+    col_table, row_table = get_table_names(db, datatable)
 
-        #SQL interface can't cope with con-current queries....
-        samp_idx = list(samp_idx)
-        var_idx = list(var_idx)
-        coords = [(var, samp) for samp in samp_idx for var in var_idx]
-        genotypes = select_by_list(allele_depth, coords)
-        data = gzip(''.join(arraybuffer.encode_array(genotypes)))
-        cache[key] = data
+    col_idx = index_from_query(db,
+                               col_table,
+                               datatable + '_column_index',
+                               col_qry,
+                               col_order)
+
+    row_idx = index_from_query(db,
+                               row_table,
+                               datatable + '_row_index',
+                               row_qry,
+                               row_order)
+    db.close()
+    hdf5_file = h5py.File(os.path.join(config.BASEDIR, '2D_data', datatable + '.hdf5'), 'r')
+    property_array = hdf5_file[property]
+    genotypes = select_by_list(property_array, col_idx, row_idx)
+    data = gzip(''.join(arraybuffer.encode_array(genotypes)))
     status = '200 OK'
     response_headers = [('Content-type', 'text/plain'),
                         ('Content-Length', str(len(data))),
