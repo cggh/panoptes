d2471bd22b7c69ff5d4513907601bad4bbfe5079
Paul Vauterin <paul.vauterin@gmail.com>
2014-01-30 18:04:24 +0000

    Boilerplate for Panoptes table whitelist

--- servermodule/uploadtracks/__init__.py
+++ servermodule/uploadtracks/__init__.py
@@ -1,1 +1,7 @@
-__author__ = 'pvaut'
+import DQXDbTools
+import config
+
+print('IMPORTING UPLOADTRACKS')
+
+# We inject some stuff that Panoptes needs to have privileges for to whitelists here
+DQXDbTools.CredentialSettings.Write_Add(config.DB,'storedviews','.*')
