0dc8267fabe1cd9c33903413eef4ed756876c2e2
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2014-02-04 14:04:39 +0000

    Remove print statement in custom module import as it is flooding the log

--- servermodule/uploadtracks/__init__.py
+++ servermodule/uploadtracks/__init__.py
@@ -2,8 +2,6 @@ import DQXDbTools
 import config
 import authorization
 
-print('Defining Panoptes specifics')
-
 # Verifies if a database operation can be done with given credentials
 
 # We set our custom credential verifier
