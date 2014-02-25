0602cb446e4312c5390970213d305fdbdb659f79
Paul Vauterin <paul.vauterin@gmail.com>
2014-02-25 14:49:17 +0000

    Make _SetServerUrl.js generic

--- webapp/scripts/Local.example/_SetServerUrl.js
+++ webapp/scripts/Local.example/_SetServerUrl.js
@@ -1,2 +1,14 @@
-serverUrl = "http://localhost:8000/app";
+
+// Automatic creation of DQXServer url in case the html file is served from a 'static' subdirectory of DQXServer
+// Note that this is the default behaviour implemented in the Panoptes build script
+if (window.location.pathname.indexOf('static/')<0) {
+    alert('ERROR: unable to determine DQXServer location. Please specify manually in _SetServerUrl.js');
+    serverUrl = '/DQXServer/app';
+}
+else {
+    serverUrl = window.location.pathname.split('static/')[0];
+}
+
+// Alternatively, one can set the DQXServer url explicitly
 //serverUrl = "/DQXServer/app";
+//serverUrl = "http://localhost:8000/app";
