1cf262bcdbd686fe471ba3fd12ce80489997932c
Paul Vauterin <paul.vauterin@gmail.com>
2014-03-17 17:19:33 +0000

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -101,7 +101,7 @@ USE ${DB};
 CREATE TABLE IF NOT EXISTS datasetindex  (
    id  varchar(20) DEFAULT NULL,
    name  varchar(50) DEFAULT NULL,
-   lastimport varchar(50) DEFAULT NULL
+   importtime varchar(50) DEFAULT NULL
 );
 CREATE TABLE IF NOT EXISTS calculations (
   id varchar(50) NOT NULL,
