766cc5b0ad84e97ba7c88c4b78770cf628f2f349
pvaut <paul.vauterin@gmail.com>
2014-04-04 10:26:40 +0100

    Show query in popup box

--- webapp/scripts/InfoPopups/DataItemTablePopup.js
+++ webapp/scripts/InfoPopups/DataItemTablePopup.js
@@ -1,10 +1,10 @@
 define(["require", "DQX/base64", "DQX/Application", "DQX/Framework", "DQX/Controls", "DQX/Msg", "DQX/SQL", "DQX/DocEl", "DQX/Utils", "DQX/QueryTable", "DQX/Map",
     "DQX/Wizard", "DQX/Popup", "DQX/PopupFrame", "DQX/ChannelPlot/GenomePlotter", "DQX/ChannelPlot/ChannelYVals", "DQX/ChannelPlot/ChannelPositions", "DQX/ChannelPlot/ChannelSequence","DQX/DataFetcher/DataFetchers", "DQX/DataFetcher/DataFetcherSummary",
-    "MetaData", "Utils/GetFullDataItemInfo", "Utils/MiscUtils", "Utils/ButtonChoiceBox"
+    "MetaData", "Utils/GetFullDataItemInfo", "Utils/MiscUtils", "Utils/ButtonChoiceBox", "Utils/QueryTool"
 ],
     function (require, base64, Application, Framework, Controls, Msg, SQL, DocEl, DQX, QueryTable, Map,
               Wizard, Popup, PopupFrame, GenomePlotter, ChannelYVals, ChannelPositions, ChannelSequence, DataFetchers, DataFetcherSummary,
-              MetaData, GetFullDataItemInfo, MiscUtils, ButtonChoiceBox
+              MetaData, GetFullDataItemInfo, MiscUtils, ButtonChoiceBox, QueryTool
         ) {
 
         var DataItemTablePopup = {};
@@ -24,8 +24,16 @@ define(["require", "DQX/base64", "DQX/Application", "DQX/Framework", "DQX/Contro
                 }
             );
 
+            that.updateQuery = function() {
+                that.myTable.setQuery(that.theQuery.getForFetching());
+                that.myTable.reLoadTable();
+            }
+
+
             that.tableInfo =MetaData.mapTableCatalog[itemInfo.tableid];
-            that.query = itemInfo.query;
+            that.theQuery = QueryTool.Create(that.tableInfo.id, {includeCurrentQuery:true, hasSubSampler:true });
+            that.theQuery.setStartQuery(itemInfo.query);
+            that.theQuery.notifyQueryUpdated = that.updateQuery;
 
             that.eventids = [];//Add event listener id's to this list to have them removed when the popup closes
             var eventid = DQX.getNextUniqueID();that.eventids.push(eventid);
@@ -37,28 +45,56 @@ define(["require", "DQX/base64", "DQX/Application", "DQX/Framework", "DQX/Contro
             } );
 
 
+
+
             that.createFrames = function() {
+
                 that.frameRoot.makeGroupVert();
 
-                that.frameBody = that.frameRoot.addMemberFrame(Framework.FrameFinal('', 0.7));
+                var frameTop = that.frameRoot.addMemberFrame(Framework.FrameGroupHor('', 0.7));
+
+                that.frameControls = frameTop.addMemberFrame(Framework.FrameFinal('', 0.3))
+                    .setAllowScrollBars(false,true);
+
+
+                that.frameBody = frameTop.addMemberFrame(Framework.FrameFinal('', 0.7));
                 that.frameButtons = that.frameRoot.addMemberFrame(Framework.FrameFinal('', 0.3))
                     .setFixedSize(Framework.dimY, 70).setFrameClassClient('DQXGrayClient');
             };
 
             that.createPanels = function() {
-                that.panelTable = MiscUtils.createDataItemTable(that.frameBody, that.tableInfo, that.query, {hasSelection: true });
+
+                var ctrl_Query = that.theQuery.createControl();
+
+                var controlsGroup = Controls.CompoundVert([
+                    ctrl_Query
+//                    Controls.VerticalSeparator(10),
+//                    that.ctrl_PointCount,
+//                    Controls.VerticalSeparator(10),
+//                    that.ctrlValueProperty,
+//                    Controls.VerticalSeparator(20),
+//                    binsizeGroup,
+//                    Controls.VerticalSeparator(10),
+//                    that.ctrl_Gamma
+                ]);
+                //that.addPlotSettingsControl('controls',controlsGroup);
+                that.panelControls = Framework.Form(that.frameControls).setPadding(5);
+                that.panelControls.addControl(controlsGroup);
+
+
+                that.panelTable = MiscUtils.createDataItemTable(that.frameBody, that.tableInfo, that.theQuery.getForFetching(), {hasSelection: true });
                 that.myTable = that.panelTable.getTable();
 
                 var button_Selection = Controls.Button(null, {content: 'Selection...', buttonClass: 'DQXToolButton2', width:100, height:40, bitmap:'Bitmaps/selection.png'}).setOnChanged(function() {
-                    ButtonChoiceBox.createQuerySelectionOptions(that.tableInfo, that.query);
+                    ButtonChoiceBox.createQuerySelectionOptions(that.tableInfo, that.theQuery.get());
                 });
 
                 var button_ShowInTableViewer = Controls.Button(null, {content: 'Show in view', buttonClass: 'DQXToolButton2', width:100, height:40, bitmap:'Bitmaps/datagrid2.png'}).setOnChanged(function() {
-                    Msg.send({type: 'ShowItemsInQuery', tableid: that.tableInfo.id}, { query: that.query });
+                    Msg.send({type: 'ShowItemsInQuery', tableid: that.tableInfo.id}, { query: that.theQuery.get() });
                 });
 
                 var button_Showplots = Controls.Button(null, {content: 'Create plot...', buttonClass: 'DQXToolButton2', width:100, height:40, bitmap:'Bitmaps/chart.png'}).setOnChanged(function() {
-                    Msg.send({type: 'CreateDataItemPlot'}, { query: that.query , tableid: that.tableInfo.id });
+                    Msg.send({type: 'CreateDataItemPlot'}, { query: that.theQuery.get() , tableid: that.tableInfo.id });
                 });
 
                 that.panelButtons = Framework.Form(that.frameButtons);
