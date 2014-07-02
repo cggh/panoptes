3a01d46800d11a378dc4a2d94728445e537f5bb1
Paul Vauterin <paul.vauterin@gmail.com>
2014-07-02 15:29:26 +0100

    Improved datatable to view action

--- webapp/scripts/InfoPopups/DataItemTablePopup.js
+++ webapp/scripts/InfoPopups/DataItemTablePopup.js
@@ -1,11 +1,11 @@
 // This file is part of Panoptes - (C) Copyright 2014, Paul Vauterin, Ben Jeffery, Alistair Miles <info@cggh.org>
 // This program is free software licensed under the GNU Affero General Public License. 
 // You can find a copy of this license in LICENSE in the top directory of the source code or at <http://opensource.org/licenses/AGPL-3.0>
-define(["require", "DQX/base64", "DQX/Application", "DQX/Framework", "DQX/Controls", "DQX/Msg", "DQX/SQL", "DQX/DocEl", "DQX/Utils", "DQX/QueryTable", "DQX/Map",
+define(["require", "DQX/base64", "DQX/Application", "DQX/Framework", "DQX/Controls", "DQX/Msg", "DQX/SQL", "DQX/DocEl", "DQX/Utils", "DQX/QueryTable", "DQX/Map", "DQX/Popup",
     "DQX/Wizard", "DQX/Popup", "DQX/PopupFrame", "DQX/ChannelPlot/GenomePlotter", "DQX/ChannelPlot/ChannelYVals", "DQX/ChannelPlot/ChannelPositions", "DQX/ChannelPlot/ChannelSequence","DQX/DataFetcher/DataFetchers", "DQX/DataFetcher/DataFetcherSummary",
     "MetaData", "Utils/GetFullDataItemInfo", "Utils/MiscUtils", "Utils/ButtonChoiceBox", "Utils/QueryTool"
 ],
-    function (require, base64, Application, Framework, Controls, Msg, SQL, DocEl, DQX, QueryTable, Map,
+    function (require, base64, Application, Framework, Controls, Msg, SQL, DocEl, DQX, QueryTable, Map, Popup,
               Wizard, Popup, PopupFrame, GenomePlotter, ChannelYVals, ChannelPositions, ChannelSequence, DataFetchers, DataFetcherSummary,
               MetaData, GetFullDataItemInfo, MiscUtils, ButtonChoiceBox, QueryTool
         ) {
@@ -125,10 +125,53 @@ define(["require", "DQX/base64", "DQX/Application", "DQX/Framework", "DQX/Contro
                 });
 
                 var button_ShowInTableViewer = Controls.Button(null, {content: 'Show in view', buttonClass: 'DQXToolButton2', width:100, height:40, bitmap:'Bitmaps/datagrid2.png'}).setOnChanged(function() {
-                    Msg.send({type: 'ShowItemsInQuery', tableid: that.tableInfo.id}, {
-                        query: that.theQuery.get(),
-                        subSamplingOptions: that.theQuery.getSubSamplingOptions()
-                    });
+                    var choices = [];
+
+
+                    var setNewQuery = function(qry) {
+                        Msg.send({type: 'ShowItemsInQuery', tableid: that.tableInfo.id}, {
+                            query: qry,
+                            subSamplingOptions: that.theQuery.getSubSamplingOptions()
+                        });
+                    };
+
+                    var currentQuery = that.tableInfo.currentQuery;
+                    if ((!currentQuery) || (currentQuery.isTrivial) ) {
+                        setNewQuery(that.theQuery.get());
+                    }
+                    else {
+                        choices.push( { content:'REPLACE<br>current query', bitmap:'Bitmaps/venn2.png', handler:function() {
+                            setNewQuery(that.theQuery.get());
+                        }
+                        });
+
+                        choices.push( { content:'ADD<br>to current query', bitmap:'Bitmaps/venn3.png', handler:function() {
+                            setNewQuery(SQL.WhereClause.OR([currentQuery, that.theQuery.get()]));
+                        }
+                        });
+
+                        choices.push( { content:'RESTRICT<br>current query', bitmap:'Bitmaps/venn1.png', handler:function() {
+                            setNewQuery(SQL.WhereClause.AND([currentQuery, that.theQuery.get()]));
+                        }
+                        });
+
+                        var content = '<div style="max-width:500px">';
+                        content += '<b>Current {name} query:</b><br>'.DQXformat({name: that.tableInfo.tableNamePlural}) + that.tableInfo.createQueryDisplayString(currentQuery);
+                        content += '<p>'
+                        content += '<b>New query:</b><br>' + that.tableInfo.createQueryDisplayString(that.theQuery.get());
+                        content += '<p>'
+                        content += '</div>';
+                        $.each(choices, function(idx, button) {
+                            var bt = Controls.Button(null, { buttonClass: 'DQXToolButton2', content: button.content, bitmap:button.bitmap, width:140, height:50 }).setOnChanged(function() {
+                                button.handler();
+                                Popup.closeIfNeeded(popupid);
+                            });
+                            content += bt.renderHtml();
+                        })
+                        var popupid = Popup.create('Show in view', content);
+                    }
+
+
                 });
 
                 var button_Showplots = Controls.Button(null, {content: 'Create plot...', buttonClass: 'DQXToolButton2', width:100, height:40, bitmap:'Bitmaps/chart.png'}).setOnChanged(function() {
