783a5ae381247e7660c695aa88c878c60a3772bc
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2014-07-04 11:33:32 +0100

    Edit the exmaple set server for new routes

--- webapp/scripts/Local.example/_SetServerUrl.js
+++ webapp/scripts/Local.example/_SetServerUrl.js
@@ -2,15 +2,10 @@
 // This program is free software licensed under the GNU Affero General Public License. 
 // You can find a copy of this license in LICENSE in the top directory of the source code or at <http://opensource.org/licenses/AGPL-3.0>
 
-// Automatic creation of DQXServer url in case the html file is served from a 'static' subdirectory of DQXServer
-// Note that this is the default behaviour implemented in the Panoptes build script
-if (window.location.pathname.indexOf('static/')<0) {
-    alert('ERROR: unable to determine DQXServer location. Please specify manually in _SetServerUrl.js');
-    serverUrl = '/DQXServer/app';
-}
-else {
-    serverUrl = window.location.pathname.split('static/')[0];
-}
+// Automatic creation of DQXServer url s
+serverUrl = window.location.pathname.split('/');
+serverUrl.pop();
+serverUrl = serverUrl.join('/') + '/api';
 
 // Alternatively, one can set the DQXServer url explicitly
 //serverUrl = "/DQXServer/app";
