39ff39ec32d2258aafd475b9445d3cd9c16c118d
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2014-07-04 14:54:01 +0100

    Add js minify script

---
+++ scripts/dist.js
@@ -0,0 +1,57 @@
+//sudo apt-get install nodejs nodejs-legacy npm
+//sudo npm install -g requirejs
+//node scripts/dist.js
+
+var requirejs = require('requirejs');
+
+var config = {
+    baseUrl: "webapp/scripts",
+    paths: {
+        jquery: "DQX/Externals/jquery",
+        d3: "DQX/Externals/d3",
+        handlebars: "DQX/Externals/handlebars",
+        markdown: "DQX/Externals/markdown",
+        DQX: "DQX",
+        _:"DQX/Externals/lodash",
+        tween: "Externals/Tween",
+        datastream: "DQX/Externals/DataStream",
+        blob: "DQX/Externals/Blob",
+        filesaver: "DQX/Externals/FileSaver"
+
+    },
+    shim: {
+        d3: {
+            exports: 'd3'
+        },
+        handlebars: {
+            exports: 'Handlebars'
+        },
+        tween: {
+          exports: 'TWEEN'
+        },
+        datastream: {
+          exports: 'DataStream'
+        },
+        blob: {
+          exports: 'Blob'
+        },
+        filesaver: {
+          exports: 'saveAs'
+        }
+
+    },
+    waitSeconds: 15,
+    name: 'main',
+    out: 'webapp/scripts/main-built.js'
+};
+
+requirejs.optimize(config, function (buildResponse) {
+    //buildResponse is just a text output of the modules
+    //included. Load the built file for the contents.
+    //Use config.out to get the optimized file contents.
+    var contents = fs.readFileSync(config.out, 'utf8');
+    console.log(contents);
+}, function(err) {
+    console.log(err);
+    //optimization err callback
+});
