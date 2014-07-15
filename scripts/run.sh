a3b783dd848f87d6d58ea71156014d49ee087740
Ben Jeffery <ben.jeffery@gmail.com>
2014-07-15 20:33:31 +0100

    Point to correct wsgi

--- scripts/run.sh
+++ scripts/run.sh
@@ -22,4 +22,4 @@ else
 	BIND=${1}
 fi  
 echo -e "${green}Serving PANOPTES on http://${BIND}/index.html${NC}"
-../virtualenv/bin/gunicorn -b ${BIND} -p ${PROJECT_ROOT}/scripts/gunicorn.pid -w 20 --access-logfile /dev/null --error-logfile - --log-level warning wsgi_static:application
+../virtualenv/bin/gunicorn -b ${BIND} -p ${PROJECT_ROOT}/scripts/gunicorn.pid -w 20 --access-logfile /dev/null --error-logfile - --log-level warning wsgi_server:application
