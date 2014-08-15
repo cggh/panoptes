54d4ff8d03b7603cdf4b995fe4d2f8b4ff0d638e
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2014-08-15 16:55:18 +0100

    Update copyright

--- webapp/scripts/Local.example/_SetAnalyticsId.js
+++ webapp/scripts/Local.example/_SetAnalyticsId.js
@@ -1,4 +1,4 @@
-// This file is part of Panoptes - (C) Copyright 2014, Paul Vauterin, Ben Jeffery, Alistair Miles <info@cggh.org>
+// This file is part of Panoptes - (C) Copyright 2014, CGGH <info@cggh.org>
 // This program is free software licensed under the GNU Affero General Public License. 
 // You can find a copy of this license in LICENSE in the top directory of the source code or at <http://opensource.org/licenses/AGPL-3.0>
 googleAnalyticsId="UA-38243754-1";
