09f081c6f1d03d36af640adcd3e085131ee7b7f1
Ben Jeffery <ben.jeffery@gmail.com>
2014-10-03 16:48:08 +0100

    Remove settings clobber

--- sampledata/datasets/vcf_example/vcf_to_panoptes.sh
+++ sampledata/datasets/vcf_example/vcf_to_panoptes.sh
@@ -12,8 +12,5 @@ vcf2npy --vcf ${vcf} --array-type calldata_2d
 vcfnpy2hdf5 --vcf ${vcf} --input-dir ${vcf}.vcfnp_cache --compression gzip --output 2D_datatables/genotypes/data.hdf5
 python -c "import h5py;print 'sample_id';print '\n'.join(h5py.File('2D_datatables/genotypes/data.hdf5')['samples'][:])" > datatables/samples/data
 
-cat > settings <<- EOF
-Name: ${vcf}
-EOF
 
 rm -rf ${vcf}.vcfnp_cache
