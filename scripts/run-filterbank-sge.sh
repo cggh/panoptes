60065e56874ef2f4475d47827e32e0c9871593ef
Ian Wright <ian.wright@well.ox.ac.uk>
2014-11-19 10:58:22 +0000

    Add comments to help understand how to configure/run

--- scripts/run-filterbank-sge.sh
+++ scripts/run-filterbank-sge.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+#Find out the available parallel environments using the command `qconf -spl`
 QUEUE_NAME=mpi_pe
-NUMPROCS=8
+NUMPROCS=14
 SCRIPT=build/DQXServer/customresponders/panoptesserver/importer/ProcessFilterBank.py
+DATASET=Samples_and_Variants
 #Find out where this script is
 SCRIPT_PATH="${BASH_SOURCE[0]}";
 if ([ -h "${SCRIPT_PATH}" ]) then
@@ -20,4 +22,25 @@ export PYTHONPATH
 
 PY=`which python`
 
-qsub -cwd -pe ${QUEUE_NAME} ${NUMPROCS} -b y `which mpirun` -x PYTHONPATH=${PROJECT_ROOT}/build/DQXServer ${PY} ${SCRIPT} all files Samples_and_Variants mpi
+qsub -cwd -pe ${QUEUE_NAME} ${NUMPROCS} -b y `which mpirun` -x PYTHONPATH=${PROJECT_ROOT}/build/DQXServer ${PY} ${SCRIPT} all files ${DATASET} mpi
+
+#If you get the message "Host key verification failed." then make sure you can ssh to the fully qualified hostname of all the relevant hosts in the queue
+#The .ssh/known_hosts file needs to contain all the hosts
+#Alternatively http://arc.liv.ac.uk/SGE/howto/hostbased-ssh.html
+
+#Example parallel environment
+#qconf -mp mpi_pe
+#pe_name            mpi_pe
+#slots              8
+#user_lists         NONE
+#xuser_lists        NONE
+#start_proc_args    /bin/true
+#stop_proc_args     /bin/true
+#allocation_rule    $fill_up
+#control_slaves     TRUE
+#job_is_first_task  FALSE
+#urgency_slots      min
+#accounting_summary FALSE
+
+#Command to modify queue
+#qconf -mq mpi.q
