e5564feb1b2ba4ba134920b5ecc1af064c89e12f
Ian Wright <ian.wright@well.ox.ac.uk>
2015-03-03 08:42:10 +0000

    Move datasetindex db schema to separate file

--- scripts/build.sh
+++ scripts/build.sh
@@ -89,36 +89,8 @@ DBPASS=`python -c "import config;print config.DBPASS"`
 DB=`python -c "import config;print config.DB"`
 mysql -h$DBSRV -u$DBUSER -p$DBPASS <<- EOF
 CREATE DATABASE IF NOT EXISTS ${DB};
-USE ${DB};
-CREATE TABLE IF NOT EXISTS datasetindex  (
-   id  varchar(255) DEFAULT NULL,
-   name  varchar(255) DEFAULT NULL,
-   importtime varchar(255) DEFAULT NULL
-);
-CREATE TABLE IF NOT EXISTS calculations (
-  id varchar(255) NOT NULL,
-  user varchar(255) DEFAULT NULL,
-  timestamp varchar(255) DEFAULT NULL,
-  name varchar(300) DEFAULT NULL,
-  status varchar(300) DEFAULT NULL,
-  progress float DEFAULT NULL,
-  completed int(11) DEFAULT NULL,
-  failed int(11) DEFAULT NULL,
-  scope varchar(255) DEFAULT NULL,
-  PRIMARY KEY (id)
-);
-CREATE TABLE IF NOT EXISTS storedviews (
-  dataset varchar(255) DEFAULT NULL,
-  workspace varchar(255) DEFAULT NULL,
-  id varchar(255) DEFAULT NULL,
-  settings text DEFAULT NULL
-);
-CREATE TABLE IF NOT EXISTS storage (
-  id varchar(255) DEFAULT NULL,
-  content text,
-  UNIQUE KEY storage_id (id)
-);
 EOF
+mysql -h$DBSRV -u$DBUSER -p$DBPASS ${DB} < scripts/datasetindex.sql
 
 BASEDIR=`python -c "import config;print config.BASEDIR"`
 echo -e "${green}  Basedir is ${BASEDIR} - making if it doesn't exist"
