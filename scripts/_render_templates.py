44a9464c110d8e698ff5baf1ec3984eaf6e56ba8
Ian Wright <ian.wright@well.ox.ac.uk>
2015-04-23 14:29:32 +0100

    Move conditional logic from template to python as it's more portable

--- scripts/_render_templates.py
+++ scripts/_render_templates.py
@@ -7,10 +7,17 @@ config = dict((k,v) for k, v in config_file.__dict__.iteritems()
               if not k.startswith('_'))
 
 config['PRODUCTION'] = sys.argv[1] == 'PRODUCTION'
-if not config['PRODUCTION']:
-    config['TITLE'] += " - DEVELOPMENT MODE"
 config['VERSION'] = sys.argv[2]
-env = Environment('<%', '%>', '<%=', '%>', '<%#', '%>', loader=FileSystemLoader('webapp'))
+if config['PRODUCTION']:
+    config['DATA_MAIN'] = 'main-built-' + config['VERSION']
+    config['DEBUG'] = 'false'
+    config['VERSION'] = '"' + config['VERSION'] + '"'
+else:
+    config['DATA_MAIN'] = 'main'
+    config['DEBUG'] = 'true'
+    config['TITLE'] += " - DEVELOPMENT MODE"
+    config['VERSION'] = 'generateUIDNotMoreThan1million()'
+env = Environment('<%', '%>', '<%=@', '%>', '<%#', '%>', loader=FileSystemLoader('webapp'))
 template = env.get_template('index.html.template')
 output_from_parsed_template = template.render(**config)
 
