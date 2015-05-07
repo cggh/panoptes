0c15cfd7170b9a0793e532ebbc7a347b80ff895b
idwright <ian.wright@well.ox.ac.uk>
2015-05-07 09:59:19 +0100

    Attempt at a Mac installer

---
+++ scripts/mac-vm-install.sh
@@ -0,0 +1,20 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+#brew tap phinze/homebrew-cask && brew install brew-cask
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install vagrant-manager
+vagrant box add ubuntu/trusty64
+brew install wget
+wget https://github.com/cggh/panoptes-boxes/archive/master.zip 
+unzip -f master.zip
+rm master.zip
+cd panoptes-boxes-master
+cd common
+./create-vendor-cookbooks.sh
+./install-vagrant-plugins.sh
+cd ../vagrant
+vagrant up
+echo "Panoptes is running at http://"`grep '"ip"' dev.json | awk -F\" '{print $4}'`
+echo "Data files are at:"`pwd`"/panoptes/current/sampledata"
+echo "To stop the vm type 'vagrant halt' in "`pwd`
+echo "To start the vm type 'vagrant up' in "`pwd`
