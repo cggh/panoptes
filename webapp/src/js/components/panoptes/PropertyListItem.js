c9790d6d14751507f6e37d43d4f77542c29c66a5
Lee Hart <lee.hart@well.ox.ac.uk>
2015-11-19 13:49:38 +0000

    Use BackgroundColor function for DataTableView and PropertyListItem
    Add rowIndex to PropertyListItem props.
    
    Add alternating row class to PropertyListItem.
    
    Add "table-col-cell" class and dynamic background color to
    PropertyListItem's PropertyCell container.

--- webapp/src/js/components/panoptes/PropertyListItem.js
+++ webapp/src/js/components/panoptes/PropertyListItem.js
@@ -12,37 +12,43 @@ const StoreWatchMixin = require('mixins/StoreWatchMixin');
 const PropertyHeader = require('panoptes/PropertyHeader');
 const PropertyCell = require('panoptes/PropertyCell');
 
-
 let PropertyListItem = React.createClass({
 
-    mixins: [
-             PureRenderMixin,
-             FluxMixin,
-             ConfigMixin
-           ],
-
-   propTypes: {
-        propertyData: React.PropTypes.object.isRequired,
-        tooltipPlacement: React.PropTypes.string.isRequired,
-        tooltipTrigger: React.PropTypes.arrayOf(React.PropTypes.string)
-   },
-
-
-      
-      
+  mixins: [
+           PureRenderMixin,
+           FluxMixin,
+           ConfigMixin
+         ],
+  
+  propTypes: {
+      propertyData: React.PropTypes.object.isRequired,
+      tooltipPlacement: React.PropTypes.string.isRequired,
+      tooltipTrigger: React.PropTypes.arrayOf(React.PropTypes.string),
+      rowIndex: React.PropTypes.number.isRequired
+  },
+  
   render: function() {
-    let {propertyData, tooltipPlacement, tooltipTrigger} = this.props;
+    let {propertyData, tooltipPlacement, tooltipTrigger, rowIndex} = this.props;
     
     let name = propertyData.name;
     let description = propertyData.description;
     let value = propertyData.value;
     
+    let {maxVal, minVal, categoryColors, showBar} = propertyData;
+    let backgroundColor = "inherit";
+    
+    let rowClass = "table-col-row-odd";
+    if (rowIndex % 2)
+    {
+      rowClass = "table-col-row-even";
+    }
+    
     return (
-       <tr>
+       <tr className={rowClass}>
          <th className="table-col-header">
            <PropertyHeader name={name} description={description} tooltipPlacement={tooltipPlacement} tooltipTrigger={tooltipTrigger} />
          </th>
-         <td><PropertyCell prop={propertyData} value={value}/></td>
+         <td className="table-col-cell" style={{backgroundColor: backgroundColor}}><PropertyCell prop={propertyData} value={value}/></td>
        </tr>
     );
     
