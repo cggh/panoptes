28bfc9c52c8e009718eef3f72df8130e712dcc84
Lee Hart <lee.hart@well.ox.ac.uk>
2015-11-20 15:53:31 +0000

    Develop DataItem tabs for FieldList and PropertyGroup

--- webapp/src/js/components/containers/DataItem.js
+++ webapp/src/js/components/containers/DataItem.js
@@ -21,6 +21,24 @@ const Loading = require('ui/Loading');
 const TabbedArea = require('ui/TabbedArea');
 const TabPane = require('ui/TabPane');
 
+
+// TODO: fix field name mangling at the source
+function rectifyString(strObj)
+{
+  /*
+    Convert {"0":"c","1":"h","2":"r","3":"o","4":"m","5":"o","6":"s","7":"o","8":"m","9":"e"}
+    Into "chromosome"
+  */
+  
+  let rectifiedString = "";
+  for (let i = 0; i < Object.keys(strObj).length; i++)
+  {
+    rectifiedString += strObj[i];
+  }
+  
+  return rectifiedString;
+}
+
 let DataItem = React.createClass({
   mixins: [
     PureRenderMixin,
@@ -32,15 +50,18 @@ let DataItem = React.createClass({
   propTypes: {
     componentUpdate: React.PropTypes.func.isRequired,
     table: React.PropTypes.string.isRequired,
-    primKey: React.PropTypes.string.isRequired
+    primKey: React.PropTypes.string.isRequired,
+    activeTab: React.PropTypes.string
   },
 
+  // TODO: if there is no activeTab, then show the Overview or the first tab.
   getInitialState() {
     return {
-      loadStatus: 'loaded'
+      loadStatus: 'loaded',
+      activeTab: 'Overview'
     };
   },
-
+  
   fetchData(props) {
     let {table, primKey} = props;
     this.setState({loadStatus: 'loading'});
@@ -72,45 +93,128 @@ let DataItem = React.createClass({
   },
 
   render() {
-    let {table, primKey, componentUpdate} = this.props;
+    let {table, primKey, componentUpdate, activeTab} = this.props;
     let {data, loadStatus} = this.state;
-
+    
+    let dataItemViews = this.config.tables[table].settings.dataItemViews;
+    let propertiesDataIndexes = {};
+    let propertiesDataUsingGroupId = {};
+    
+    // Make a clone of the propertiesData
     let propertiesData = _.cloneDeep(this.config.tables[table].properties);
-    let propertyListTitle = "Overview";
-
-    let tabArea = null;
     if (data)
     {
-      // Augment the propertiesData array with each property's value
       for (let i = 0; i < propertiesData.length; i++)
       {
+        // Augment the array element with the fetched value of the property.
         propertiesData[i].value = data[propertiesData[i].propid];
+        
+        // Record which property each propertiesData index relates to.
+        propertiesDataIndexes[propertiesData[i].propid] = i;
+        
+        // Record which properties are in each property group.
+        if (typeof propertiesDataUsingGroupId[propertiesData[i].settings.groupId] == 'undefined')
+        {
+          propertiesDataUsingGroupId[propertiesData[i].settings.groupId] = [];
+        }
+        propertiesDataUsingGroupId[propertiesData[i].settings.groupId].push(propertiesData[i]);
+        
       }
-
-      tabArea =  (
-                  <TabbedArea activeTab={"something"} >
-                    <TabPane compId={"something"} >
-                        <PropertyList title={propertyListTitle} propertiesData={propertiesData} className='table-col' />
-                    </TabPane>
-                  </TabbedArea>
-      )
     }
-    if (loadStatus == 'loaded' && !tabArea)
-      return (
-        <div>
-          <Loading status="custom">No properties data</Loading>
-        </div>
-      );
+    
+    let tabPanes = [];
+    
+    if (!dataItemViews)
+    {
+      if (data)
+      {
+        let tabPane = (
+          <TabPane key="0" compId="overview" >
+              <PropertyList title="Overview" propertiesData={propertiesData} className='table-col' />
+          </TabPane>
+        );
+        
+        tabPanes.push(tabPane);
+      }
+    }
     else
-      return (
-      <div>
-        {tabArea}
-        <Loading status={loadStatus}/>
-      </div>
-
-    );
+    {
+      for (let i = 0; i < dataItemViews.length; i++)
+      {
+        let tabPaneCompId = dataItemViews[i].name;
+        let tabPaneContents = null;
+        
+        if (dataItemViews[i].type === "Overview" && data)
+        {
+          tabPaneContents = (
+            <PropertyList title={dataItemViews[i].name} propertiesData={propertiesData} className='table-col' />
+          )
+        }
+        if (dataItemViews[i].type === "PieChartMap")
+        {
+          tabPaneContents = (
+            <div>PieChartMap, TODO</div>
+          )
+        }
+        if (dataItemViews[i].type === "ItemMap")
+        {
+          tabPaneContents = (
+            <div>ItemMap, TODO</div>
+          )
+        }
+        if (dataItemViews[i].type === "FieldList" && data)
+        {
+          let fieldListPropertiesData = [];
+          for (let j = 0; j < dataItemViews[i].fields.length; j++)
+          {
+            // TODO: fix field name mangling at the source
+            let rectifiedFieldName = rectifyString(dataItemViews[i].fields[j]);
+            
+            fieldListPropertiesData.push(propertiesData[propertiesDataIndexes[rectifiedFieldName]]);
+          }
+          
+          tabPaneContents = (
+            <PropertyList title={dataItemViews[i].name} propertiesData={fieldListPropertiesData} className='table-col' />
+          )
+        }
+        if (dataItemViews[i].type === "PropertyGroup" && data)
+        {
+          tabPaneContents = (
+            <PropertyList title={dataItemViews[i].name} propertiesData={propertiesDataUsingGroupId[dataItemViews[i].groupId]} className='table-col' />
+          )
+        }
+        if (dataItemViews[i].type === "Template")
+        {
+          tabPaneContents = (
+            <div>Template, TODO</div>
+          )
+        }
+        
+        if (tabPaneContents)
+        {
+          let tabPane = (
+            <TabPane key={i} compId={tabPaneCompId} >
+                {tabPaneContents}
+            </TabPane>
+          );
+        
+          tabPanes.push(tabPane);
+        }
+        
+      }
+    }
+    
+    return (
+        <div>
+          <TabbedArea activeTab={activeTab} onSwitch={(id) => componentUpdate({activeTab:id})} >
+            {tabPanes}
+          </TabbedArea>
+          <Loading status={loadStatus}/>
+        </div>
+    )
+    
   }
-
+  
 });
 
 module.exports = DataItem;
