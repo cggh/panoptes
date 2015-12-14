995b42c5d4e87774c8811857e24a80624c6407d9
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2015-12-14 16:28:44 +0000

    Add more rules

--- webapp/.eslintrc.js
+++ webapp/.eslintrc.js
@@ -17,7 +17,42 @@ module.exports = {
       2,
       'always'
     ],
-    'no-console': 0
+
+    'no-console': 0,
+
+    'array-bracket-spacing': 2,
+    'block-spacing': 2,
+    'brace-style': [2, '1tbs', {'allowSingleLine': true}],
+    'camelcase': 2,
+    'comma-spacing': 2,
+    'comma-style': 2,
+    'eol-last': 2,
+    'key-spacing': 2,
+    'no-spaced-func': 2,
+    'no-trailing-spaces': 2,
+    'object-curly-spacing': 2,
+    'space-after-keywords': 2,
+    'space-before-blocks': 2,
+    'space-before-function-paren': [2, 'never'],
+    'space-before-keywords': 2,
+    'space-infix-ops': [2, {'int32Hint': true}],
+    'space-return-throw-case': 2,
+    'space-unary-ops': 2,
+
+    'jsx-quotes': [
+      2,
+      'prefer-double'
+    ],
+
+    'new-cap': 2,
+    'arrow-body-style': 2,
+    'arrow-parens': 2,
+    'arrow-spacing': 2,
+    'no-const-assign': 2,
+    'no-dupe-class-members': 2,
+    'no-var': 2,
+    'prefer-arrow-callback': 2
+
   },
   'env': {
     'es6': true,
