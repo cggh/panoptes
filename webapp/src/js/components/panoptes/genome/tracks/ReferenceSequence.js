6afac5be69b50dc7b3f1a070f4a9205a93b5426d
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2016-02-10 14:50:55 +0000

    Letters on sequence track

--- webapp/src/js/components/panoptes/genome/tracks/ReferenceSequence.js
+++ webapp/src/js/components/panoptes/genome/tracks/ReferenceSequence.js
@@ -66,20 +66,20 @@ let ReferenceSequence = React.createClass({
     requestContext.request(
       (componentCancellation) =>
         SummarisationCache.fetch({
-          columns: {
-            sequence: {
-              folder: `SummaryTracks/${this.config.dataset}/Sequence`,
-              config: 'Summ',
-              name: 'Base_avg'
-            }
-          },
-          minBlockSize: 1,
-          chromosome: chromosome,
-          start: block1Start,
-          end: block1End,
-          targetPointCount: targetPointCount,
-          cancellation: componentCancellation
-        })
+            columns: {
+              sequence: {
+                folder: `SummaryTracks/${this.config.dataset}/Sequence`,
+                config: 'Summ',
+                name: 'Base_avg'
+              }
+            },
+            minBlockSize: 1,
+            chromosome: chromosome,
+            start: block1Start,
+            end: block1End,
+            targetPointCount: targetPointCount,
+            cancellation: componentCancellation
+          })
           .then((data) => {
             this.props.onChangeLoadStatus('DONE');
             this.applyData(data);
@@ -107,19 +107,56 @@ let ReferenceSequence = React.createClass({
         sideWidth={sideWidth}
         sideComponent={<div className="side-name">Ref. Seq.</div>}
       >
-            <SequenceSquares
-              width={width - sideWidth}
-              height={HEIGHT}
-              start={start}
-              end={end}
-              dataStart={dataStart}
-              dataStep={dataStep}
-              sequence={sequence}/>
-        </Channel>
+        <div className="sequence">
+          <SequenceSquares
+            width={width - sideWidth}
+            height={HEIGHT}
+            start={start}
+            end={end}
+            dataStart={dataStart}
+            dataStep={dataStep}
+            sequence={sequence}/>
+          <SequenceText
+            width={width - sideWidth}
+            height={HEIGHT}
+            start={start}
+            end={end}
+            dataStart={dataStart}
+            dataStep={dataStep}
+            sequence={sequence}/>
+        </div>
+      </Channel>
     );
   }
 
 });
+
+let SequenceText = React.createClass({
+  mixins: [
+    PureRenderMixin
+  ],
+
+  render() {
+    let {width, height, start, end, dataStart, dataStep, sequence} = this.props;
+    if (!sequence)
+      return null;
+    let startIndex = Math.max(0, Math.floor((start - dataStart) / dataStep));
+    let endIndex = Math.min(sequence.length - 1, Math.ceil((end - dataStart) / dataStep));
+    let scale = d3.scale.linear().domain([start, end]).range([0, width]);
+    //Don't draw text if no room
+    if (scale(1) - scale(0) < 15)
+      return null;
+    return <svg  viewBox={`0 ${-height / 2} ${width} ${height}`} width={width} height={height}>
+      {sequence.slice(startIndex, endIndex).map(
+        (char, i) => {
+          let pos = dataStart + ((i + startIndex) * dataStep)+0.5;
+          return <text key={pos} x={scale(pos)}>{char}</text>;
+        }
+      )}
+    </svg>;
+  }
+});
+
 
 let SequenceSquares = React.createClass({
   mixins: [
@@ -181,7 +218,6 @@ let SequenceSquares = React.createClass({
     let stepWidth = scale(dataStep) - scale(0);
     let offset = scale(dataStart) - scale(start);
     return <canvas ref="canvas"
-                   className="sequence-canvas"
                    style={{transform: `translateX(${offset}px) scale(${stepWidth},${height})`}}
                    width={sequence.length}
                    height={1}/>;
