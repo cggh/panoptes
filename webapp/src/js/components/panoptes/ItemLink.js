b0e1019cb4c5dac8d2e2ca29dcacdad6d7ef6d10
Lee Hart <lee.hart@well.ox.ac.uk>
2016-04-05 12:14:09 +0100

    Add onClick action to ItemLink

--- webapp/src/js/components/panoptes/ItemLink.js
+++ webapp/src/js/components/panoptes/ItemLink.js
@@ -1,15 +1,23 @@
 import React from 'react';
+
+// Mixins
 import PureRenderMixin from 'mixins/PureRenderMixin';
+import FluxMixin from 'mixins/FluxMixin';
 
 let ItemLink = React.createClass({
 
-  mixins: [PureRenderMixin],
+  mixins: [
+    PureRenderMixin,
+    FluxMixin
+  ],
 
   render() {
     let {table, primKey} = this.props;
-    // TODO: How is the href meant to be constructed?
+
+    let actions = this.getFlux().actions;
+
     return (
-      <a href={table + '?' + primKey}>
+      <a href="javascript:void(0)" onClick={() => actions.panoptes.dataItemPopup({table: table, primKey: primKey.toString()})}>
         {primKey}
       </a>
     );
