cbc588843d94d9239e26fcbd6dc2fb61e3dd7b9c
Lee Hart <lee.hart@well.ox.ac.uk>
2016-04-14 18:10:42 +0100

    Develop Finder and GeneFinder

--- webapp/src/js/components/containers/Finder.js
+++ webapp/src/js/components/containers/Finder.js
@@ -3,6 +3,7 @@ import React from 'react';
 // Mixins
 import PureRenderMixin from 'mixins/PureRenderMixin';
 import FluxMixin from 'mixins/FluxMixin';
+import ConfigMixin from 'mixins/ConfigMixin';
 
 // Material UI
 import List from 'material-ui/lib/lists/list';
@@ -10,10 +11,14 @@ import ListItem from 'material-ui/lib/lists/list-item';
 
 import Icon from 'ui/Icon';
 
+// lodash
+import _keys from 'lodash/keys';
+
 let Finder = React.createClass({
   mixins: [
     PureRenderMixin,
-    FluxMixin
+    FluxMixin,
+    ConfigMixin
   ],
 
   getDefaultProps() {
@@ -30,24 +35,72 @@ let Finder = React.createClass({
     return this.props.title;
   },
 
-  handleClick({container, props, middleClick}) {
+  handleSwitchModal(container, props) {
     this.getFlux().actions.session.modalClose();
-    this.getFlux().actions.session.modalOpen('containers/GeneFinder', {});
+    this.getFlux().actions.session.modalOpen(container, props);
   },
 
   render() {
-    return (
-      <List>
-        <ListItem primaryText="Find gene"
-                  leftIcon={<div><Icon fixedWidth={true} name="bitmap:genomebrowser.png"/></div>}
-                  onClick={this.handleClick}
+
+    // TODO: A vertical list is not entirely scalable (modal dialog would go off screen).
+
+    let listItems = [];
+
+    if (this.config.settings.hasGenomeBrowser) {
+
+      let listItem = (
+        <ListItem key="geneFinderListItem"
+                  primaryText="Gene"
+                  leftIcon={<div><Icon fixedWidth={true} name="bitmap:genomebrowser.png" /></div>}
+                  onClick={() => this.handleSwitchModal('containers/GeneFinder', {})}
         />
-        <ListItem primaryText="Find sample"
-                  leftIcon={<div><Icon fixedWidth={true} name="flask"/></div>}
-                  onClick={this.handleClick}
-        />
-      </List>
-    );
+      );
+
+      listItems.push(listItem);
+    }
+
+    if (_keys(this.config.tables).length > 0) {
+
+      let tables = this.config.tables;
+
+      // TODO: Only show tables that should be shown here. Determined by what?
+console.log('this.config: %o', this.config);
+
+      for (let table in tables) {
+
+        let listItem = (
+          <ListItem key={tables[table].id}
+                    primaryText={tables[table].tableCapNameSingle}
+                    leftIcon={<div><Icon fixedWidth={true} name={tables[table].icon} /></div>}
+                    onClick={() => this.handleSwitchModal('containers/DatumFinder', {table: table})}
+          />
+        );
+
+        listItems.push(listItem);
+      }
+
+    }
+
+    let finderContent = null;
+
+    if (listItems.length > 0) {
+      finderContent = (
+        <List>
+          {listItems}
+        </List>
+      );
+    } else {
+
+      // TODO: Would be better to also disable the Find button.
+
+      finderContent = (
+        <div className="centering-container">
+          No search wizards available.
+        </div>
+      );
+    }
+
+    return finderContent;
   }
 });
 
--- webapp/src/js/components/containers/GeneFinder.js
+++ webapp/src/js/components/containers/GeneFinder.js
@@ -7,6 +7,8 @@ import FluxMixin from 'mixins/FluxMixin';
 // Material UI
 import List from 'material-ui/lib/lists/list';
 import ListItem from 'material-ui/lib/lists/list-item';
+import Subheader from 'material-ui/lib/Subheader';
+import RaisedButton from 'material-ui/lib/raised-button';
 
 import Icon from 'ui/Icon';
 
@@ -19,7 +21,14 @@ let GeneFinder = React.createClass({
   getDefaultProps() {
     return {
       title: 'Find gene',
-      icon: 'bitmap:genomebrowser.png'
+      icon: 'bitmap:genomebrowser.png',
+      initialPane: null
+    };
+  },
+
+  getInitialState() {
+    return {
+      pane: this.props.initialPane
     };
   },
 
@@ -30,24 +39,68 @@ let GeneFinder = React.createClass({
     return this.props.title;
   },
 
-  handleClick({container, props, middleClick}) {
-    this.getFlux().actions.session.modalClose();
-    this.getFlux().actions.session.modalOpen('containers/GeneFinder', {});
+  handleSwitchPane(container, props) {
+
+    this.setState(props);
+
   },
 
   render() {
-    return (
-      <List>
-        <ListItem primaryText="Search by name or description"
-                  leftIcon={<div><Icon fixedWidth={true} name="bitmap:genomebrowser.png" /></div>}
-                  onClick={this.handleClick}
-        />
-        <ListItem primaryText="Search by genomic region"
-                  leftIcon={<div><Icon fixedWidth={true} name="bitmap:genomebrowser.png" /></div>}
-                  onClick={this.handleClick}
-        />
-      </List>
-    );
+
+    let {pane} = this.state;
+
+    let geneFinderContent = null;
+
+    if (pane === null) {
+
+      geneFinderContent = (
+        <List>
+          <Subheader>Search by:</Subheader>
+          <ListItem primaryText="Gene name / description"
+                    leftIcon={<div><Icon fixedWidth={true} name="bitmap:genomebrowser.png" /></div>}
+                    onClick={() => this.handleSwitchPane('containers/GeneFinder', {pane: 'search by name or description'})}
+          />
+          <ListItem primaryText="Genomic region"
+                    leftIcon={<div><Icon fixedWidth={true} name="bitmap:genomebrowser.png" /></div>}
+                    onClick={() => this.handleSwitchPane('containers/GeneFinder', {pane: 'search by genomic region'})}
+          />
+        </List>
+      );
+
+    } else if (pane === 'search by name or description') {
+
+      geneFinderContent = (
+        <div>
+          <div className="centering-container">
+            <p>search by name or description</p>
+          </div>
+          <div className="centering-container">
+            <RaisedButton label={<span>Start again</span>}
+                          primary={true}
+                          onClick={() => this.handleSwitchPane('containers/GeneFinder', {pane: null})}
+            />
+          </div>
+        </div>
+      );
+
+    } else {
+      console.error('Unhandled pane: ' + pane);
+      geneFinderContent = (
+        <div>
+          <div className="centering-container">
+            <p>Error: Unhandled pane</p>
+          </div>
+          <div className="centering-container">
+            <RaisedButton label={<span>Start again</span>}
+                          primary={true}
+                          onClick={() => this.handleSwitchPane('containers/GeneFinder', {pane: null})}
+            />
+          </div>
+        </div>
+      );
+    }
+
+    return geneFinderContent;
   }
 });
 
