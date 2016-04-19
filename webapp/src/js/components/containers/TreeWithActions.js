d3f420ffb8bfaa82b0f8d0cf1128197b9be2d436
Ben Jeffery <ben.jeffery@gmail.com>
2016-04-19 11:27:56 +0100

    Spike tree tab

---
+++ webapp/src/js/components/containers/TreeWithActions.js
@@ -0,0 +1,144 @@
+import React from 'react';
+import PureRenderMixin from 'mixins/PureRenderMixin';
+
+import _map from 'lodash/map';
+import _each from 'lodash/map';
+import _filter from 'lodash/filter';
+import scrollbarSize from 'scrollbar-size';
+
+import ConfigMixin from 'mixins/ConfigMixin';
+import FluxMixin from 'mixins/FluxMixin';
+
+import Sidebar from 'react-sidebar';
+import SidebarHeader from 'ui/SidebarHeader';
+
+import Icon from 'ui/Icon';
+import SQL from 'panoptes/SQL';
+import QueryString from 'panoptes/QueryString';
+
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import Divider from 'material-ui/Divider';
+
+import {FlatButton} from 'material-ui';
+
+import 'tree.scss';
+
+let TreeWithActions = React.createClass({
+  mixins: [
+    PureRenderMixin,
+    ConfigMixin,
+    FluxMixin
+  ],
+
+  propTypes: {
+    componentUpdate: React.PropTypes.func.isRequired,
+    title: React.PropTypes.string,
+    sidebar: React.PropTypes.bool,
+    table: React.PropTypes.string,
+  },
+
+  getDefaultProps() {
+    return {
+      componentUpdate: null,
+      sidebar: true
+    };
+  },
+
+  icon() {
+    return 'tree';
+  },
+
+  title() {
+    return this.props.title || 'Tree';
+  },
+
+
+  render() {
+    console.log(this.config);
+    let {sidebar, table, query, column, componentUpdate} = this.props;
+    const actions = this.getFlux().actions;
+
+
+    let tables = _map(_filter(this.config.tables, 'hasGeoCoord'),
+      (val) => ({
+        payload: val.id,
+        icon: <Icon fixedWidth={true} name={val.icon}/>,
+        text: (<div className="dropdown-option">{val.tableCapNamePlural}</div>)
+      }));
+
+    let propertyMenu = [];
+    let i = 0;
+    if (table) {
+      const propertyGroups = this.config.tables[table].propertyGroups;
+      _each(propertyGroups, (group) => {
+        if (propertyMenu.length) {
+          propertyMenu.push(<Divider key={i++}/>);
+        }
+        let {id, name} = group;
+        propertyMenu.push(<MenuItem disabled value={id} key={id} primaryText={name}/>);
+        _each(group.properties, (property) => {
+          let {propid, name} = property;
+          propertyMenu.push(<MenuItem value={propid} key={propid} primaryText={name}/>);
+        });
+      });
+    }
+    let sidebarContent = (
+      <div className="sidebar tree-sidebar">
+        <SidebarHeader icon={this.icon()} description="Something here"/>
+        <div className="tree-controls vertical stack">
+          <SelectField value={table}
+                       autoWidth={true}
+                       floatingLabelText="Table:"
+                       onChange={(e, i, v) => componentUpdate({table: v})}>
+            {tables.map(({payload, text, icon}) =>
+              <MenuItem value={payload} key={payload} leftIcon={icon} primaryText={text}/>)}
+          </SelectField>
+          {table ? <FlatButton label="Change Filter"
+                               primary={true}
+                               onClick={() => actions.session.modalOpen('containers/QueryPicker',
+                      {
+                        table: table,
+                        initialQuery: query,
+                        onPick: this.handleQueryPick
+                      })}/>
+            : null}
+          {table ? <FlatButton label="Clear Filter"
+                               primary={true}
+                               onClick={() => componentUpdate({query: SQL.NullQuery})}/>
+            : null}
+          {table ?
+            <SelectField value={this.config.tables[table].propertiesMap[column] ? column : null}
+                         autoWidth={true}
+                         floatingLabelText="Column"
+                         onChange={(e, i, v) => componentUpdate({column: v})}>
+              {propertyMenu}
+            </SelectField>
+            : null }
+        </div>
+      </div>
+    );
+    return (
+      <Sidebar
+        docked={sidebar}
+        styles={{sidebar: {paddingRight: `${scrollbarSize()}px`}}}
+        sidebar={sidebarContent}>
+        <div className="vertical stack">
+          <div className="top-bar">
+            <Icon className="pointer icon"
+                  name={sidebar ? 'arrow-left' : 'bars'}
+                  onClick={() => componentUpdate({sidebar: !sidebar})}/>
+            <span className="text">{table ? `Tree of ${this.config.tables[table].tableCapNamePlural}` : 'Tree'}</span>
+            {table ?
+              <span className="block text">
+                <QueryString prepend="Filter:" table={table} query={query}/>
+              </span>
+              : null}
+          </div>
+        </div>
+      </Sidebar>
+    );
+  }
+});
+
+module.exports = TreeWithActions;
