a8c7ebd915944bf088bdf5ad85eb8240f0629d8b
Ben Jeffery <ben.jeffery@gmail.com>
2016-05-03 16:46:19 +0100

    Add missing PropertySelector

---
+++ webapp/src/js/panoptes/PropertySelector.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ConfigMixin from 'mixins/ConfigMixin';
+import PureRenderWithRedirectedProps from 'mixins/PureRenderWithRedirectedProps';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+
+
+const PropertySelector = React.createClass({
+  mixins: [
+    PureRenderWithRedirectedProps({
+      redirect: [
+        'onSelect'
+      ],
+      check: [
+        'table',
+        'value'
+      ]
+    }),
+    ConfigMixin
+  ],
+
+  propTypes: {
+    table: React.PropTypes.string,
+    value: React.PropTypes.string,
+    onSelect: React.PropTypes.func
+  },
+
+  render() {
+    const { table, value } = this.props;
+    return (
+      <DropDownMenu className="dropdown"
+                  value={value}
+                  onChange={(e, i, v) => this.redirectedProps.onSelect(v)}>
+        <MenuItem key="__none__" value={undefined} primaryText="None"/>
+        {this.config.tables[table].properties.map((property) =>
+          <MenuItem key={property.propid} value={property.propid} primaryText={property.name}/>)}
+      </DropDownMenu>
+    );
+  }
+});
+
+export default PropertySelector;
