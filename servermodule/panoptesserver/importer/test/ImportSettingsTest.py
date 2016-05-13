3b881b0286587d4d426dfa90bdcf9b0fe9ecf281
idwright <ian.wright@well.ox.ac.uk>
2016-05-13 15:35:56 +0100

    Fix tests

--- servermodule/panoptesserver/importer/test/ImportSettingsTest.py
+++ servermodule/panoptesserver/importer/test/ImportSettingsTest.py
@@ -1,6 +1,6 @@
 import unittest
 import os
-import ImportSettings
+from SettingsDataTable import SettingsDataTable
 import simplejson
 
 ''' Not doing it anymore....
@@ -12,7 +12,7 @@ import simplejson
 		testProps = self._testProps
 		testProps['Properties'][0]['IsCategorical'] = False
 		for val in valuest:
-			settingsLoaded = ImportSettings.ImportSettings()
+			settingsLoaded = SettingsDataTable()
 			testProps['Properties'][0]['IsCategorical'] = val
 			settingsLoaded.loadProps(testProps)
 			loaded = settingsLoaded.getPropertyValue('Test',"IsCategorical")
@@ -21,7 +21,7 @@ import simplejson
 
 			
 		for val in valuesf:
-			settingsLoaded = ImportSettings.ImportSettings()
+			settingsLoaded = SettingsDataTable()
 			testProps['Properties'][0]['IsCategorical'] = val
 			settingsLoaded.loadProps(testProps)
 			self.assertIs(type(settingsLoaded.getPropertyValue('Test',"IsCategorical")), bool)
@@ -46,7 +46,7 @@ class ImportSettingsTest(unittest.TestCase):
 										}]}
 	
 	def testMergeProperties(self):
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps = self._testProps
 		testProps['Properties'].append({ "Id": "Test1",
 									"Name": "Test1"})
@@ -64,7 +64,7 @@ class ImportSettingsTest(unittest.TestCase):
 	#DefineKnownTokens seems to convert the case of properties
 	def testKnownTokens(self):
 	
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps = self._testProps
 		testProps['Properties'][0]['NotAProp'] = False
 		
@@ -73,7 +73,7 @@ class ImportSettingsTest(unittest.TestCase):
 														
 	def testSetDefaultProperty(self):
 		
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps = self._testProps
 		
 		settingsLoaded.loadProps(testProps)
@@ -82,7 +82,7 @@ class ImportSettingsTest(unittest.TestCase):
 
 	def testPropertyEnum(self):
 		
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps = self._testProps
 		
 		testProps["Properties"][0]["Search"] = 'NotAVal'
@@ -91,7 +91,7 @@ class ImportSettingsTest(unittest.TestCase):
 
 	def testPropertyType(self):
 		
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps = self._testProps
 		
 		testProps["Properties"][0]["MinVal"] = 'NotAnInt'
@@ -100,29 +100,29 @@ class ImportSettingsTest(unittest.TestCase):
 		
 	def testPropertyId(self):
 		
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps = self._testProps
 		
 		testProps["PrimKey"] = 'Test'
 		settingsLoaded.loadProps(testProps)
 		
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps["PrimKey"] = 'AutoKey'
 		settingsLoaded.loadProps(testProps)
 		
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps["PrimKey"] = 'NotAProp'
 		self.assertRaises(ValueError, settingsLoaded.loadProps, testProps)
 
 	def testPropertyIds(self):
 		
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps = self._testProps
 		
 		testProps["QuickFindFields"] = 'Test,TestA'
 		settingsLoaded.loadProps(testProps)
 		
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps["QuickFindFields"] = 'Test,NotAProp'
 		
 		self.assertRaises(ValueError, settingsLoaded.loadProps, testProps)
@@ -130,24 +130,24 @@ class ImportSettingsTest(unittest.TestCase):
 		
 	def testRequiredProperty(self):
 		
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps = self._testProps
 		del testProps["Properties"][0]["DataType"]
 		self.assertRaises(ValueError, settingsLoaded.loadProps, testProps)
 		
 	def testRequiredChildProperty(self):
 		
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps = self._testProps
 		testProps["Properties"][0]["Relation"] = 'NotAVal'
 		self.assertRaises(ValueError, settingsLoaded.loadProps, testProps)
 		
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps["Properties"][0]["Relation"] = {}
 		self.assertRaises(ValueError, settingsLoaded.loadProps, testProps)
 
 	def testGetSetting(self):
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps = self._testProps
 		
 		settingsLoaded.loadProps(testProps)
@@ -155,7 +155,7 @@ class ImportSettingsTest(unittest.TestCase):
 		self.assertEquals(testProps['NamePlural'],settingsLoaded['NamePlural'])
 
 	def testGetDefaultSetting(self):
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps = self._testProps
 		
 		settingsLoaded.loadProps(testProps)
@@ -164,29 +164,29 @@ class ImportSettingsTest(unittest.TestCase):
 		
 		
 	def testSiblingOptional(self):
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps = self._testProps
 		
 		testProps["DataItemViews"] = [ { 'Type': 'FieldList', 'Name': 'TestGroup', 'Introduction': 'Intro text', 'Fields': [ 'Test' ]} ]
 		settingsLoaded.loadProps(testProps)
 		
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps["DataItemViews"] = [ { 'Type': 'FieldList', 'Name': 'TestGroup', 'Fields': [ 'Test' ] } ]
 		settingsLoaded.loadProps(testProps)
 		
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps["DataItemViews"] = [ { 'Type': 'PropertyGroup', 'Name': 'TestGroup', 'Introduction': 'Intro text', 'Fields': [ 'Test' ] } ]
 		#Should raise an error for extra field 'Fields'
 		self.assertRaises(ValueError, settingsLoaded.loadProps, testProps)
 
 	def testSiblingValue(self):
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps = self._testProps
 		
 		testProps['Properties'].append({ "Id": "TestSV",
 									"Name": "TestSV",
 									"DataType": "Value",
-									"BarWidth": 180})
+									"ShowBar": True})
 		
 		settingsLoaded.loadProps(testProps)
 		
@@ -194,38 +194,38 @@ class ImportSettingsTest(unittest.TestCase):
 		
 
 	def testSiblingRequired(self):
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps = self._testProps
 		
 		testProps["DataItemViews"] = [ { 'Type': 'PropertyGroup', 'Name': 'TestGroup', 'GroupId': 'TestGroupId'} ]
 		settingsLoaded.loadProps(testProps)
 				
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps["DataItemViews"] = [ { 'Type': 'PropertyGroup', 'Name': 'TestGroup', 'GroupId': 'TestGroupId', 'MapZoom': 2} ]
 		self.assertRaises(ValueError, settingsLoaded.loadProps, testProps)
 		
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps["DataItemViews"] = [ { 'Type': 'PropertyGroup', 'Name': 'TestGroup' } ]
 		#Should raise an error for no GroupId
 		self.assertRaises(ValueError, settingsLoaded.loadProps, testProps)
 
 		
 	def testSettingRequired(self):
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps = self._testProps
 		testProps['IsPositionOnGenome'] = True
 		#Should raise an error because no Chromosome or Position
 		self.assertRaises(ValueError, settingsLoaded.loadProps, testProps)
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps['Chromosome'] = 'TestA'
 		self.assertRaises(ValueError, settingsLoaded.loadProps, testProps)
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps['Position'] = 'Test'
 		settingsLoaded.loadProps(testProps)
 	
 	def testSerialize(self):
 
-		settingsLoaded = ImportSettings.ImportSettings()
+		settingsLoaded = SettingsDataTable()
 		testProps = self._testProps
 		testProps['Properties'].append({ "Id": "Test1",
 									"Name": "Test1",
@@ -237,11 +237,10 @@ class ImportSettingsTest(unittest.TestCase):
 		settingsLoaded.loadProps(testProps)
 		prop = settingsLoaded.serializeProperty("Test1")
 		propDict = simplejson.loads(prop, strict=False)
-		self.assertIn('maxval', propDict, 'maxval default value should be set')
+		self.assertIn('Search', propDict, 'Search default value should be set')
 		self.assertNotIn('Name', propDict, 'Name should not be serialized')
 		prop = settingsLoaded.serializeProperty("Test2")
 		propDict = simplejson.loads(prop, strict=False)
-		self.assertNotIn('maxval', propDict, 'maxval default value should not be set')
 		self.assertNotIn('IsCategorical', propDict, 'propName not working - old value present')
 		self.assertIn('isCategorical', propDict, 'propName not working - new value not present')
 		
