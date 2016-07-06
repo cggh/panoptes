feefd2482d250a0047f151413a3210dfb89a78dc
Ben Jeffery <ben.jeffery@gmail.com>
2016-07-07 00:39:06 +0100

    setconfig WIP

---
+++ servermodule/panoptesserver/setconfig.py
@@ -0,0 +1,20 @@
+# This file is part of Panoptes - (C) Copyright 2014, CGGH <info@cggh.org>
+# This program is free software licensed under the GNU Affero General Public License.
+# You can find a copy of this license in LICENSE in the top directory of the source code or at <http://opensource.org/licenses/AGPL-3.0>
+
+import DQXDbTools
+import json
+from importer import readConfig
+
+def response(returndata):
+    path = returndata['path']
+    try:
+        length = int(returndata['environ'].get('CONTENT_LENGTH', '0'))
+    except ValueError:
+        length = 0
+    content = json.loads((returndata['environ']['wsgi.input'].read(length)))
+    DQXDbTools.CredentialInformation(returndata).VerifyCanDo(DQXDbTools.DbOperationRead(returndata['dataset']))
+
+    #Based on path load a subset of settings then merge in new, verify and save
+
+    return returndata
