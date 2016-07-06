dae857d361f1112863858c287842471449ba4d62
Ben Jeffery <ben.jeffery@gmail.com>
2016-07-06 23:57:21 +0100

    Check for permissions for config

--- servermodule/panoptesserver/getconfig.py
+++ servermodule/panoptesserver/getconfig.py
@@ -2,14 +2,11 @@
 # This program is free software licensed under the GNU Affero General Public License.
 # You can find a copy of this license in LICENSE in the top directory of the source code or at <http://opensource.org/licenses/AGPL-3.0>
 
-import config
-import os
-import uuid
 import DQXDbTools
 import authorization
-import schemaversion
 from importer import readConfig
 
 def response(returndata):
+    DQXDbTools.CredentialInformation(returndata).VerifyCanDo(DQXDbTools.DbOperationRead(returndata['dataset']))
     returndata['config'] = readConfig.readJSONConfig(returndata['dataset'])
     return returndata
