f9caf0bd60b429fa0c85bf7310c3b91b7d20c46e
Ben Jeffery <ben.jeffery@gmail.com>
2016-07-22 00:18:52 +0100

    Add filtering to PropertySelector

--- webapp/src/js/panoptes/PropertySelector.js
+++ webapp/src/js/panoptes/PropertySelector.js
@@ -5,7 +5,8 @@ import FluxMixin from 'mixins/FluxMixin';
 import Divider from 'material-ui/Divider';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
-import _each from 'lodash/map';
+import _each from 'lodash/each';
+import _filter from 'lodash/filter';
 
 
 const PropertySelector = React.createClass({
@@ -26,23 +27,32 @@ const PropertySelector = React.createClass({
   propTypes: {
     table: React.PropTypes.string,
     value: React.PropTypes.string,
+    filter: React.PropTypes.func,
     onSelect: React.PropTypes.func
   },
 
+  getDefaultProps() {
+    return {
+      filter: () => true
+    };
+  },
+
   render() {
-    const { table, value, label} = this.props;
+    const { table, value, label, filter} = this.props;
 
     let propertyMenu = [];
     let i = 0;
     if (table) {
       const propertyGroups = this.config.tablesById[table].propertyGroups;
       _each(propertyGroups, (group) => {
-        if (propertyMenu.length) {
+        let filteredProps = _filter(group.properties, filter);
+        if (filteredProps.length == 0) return;
+        if (propertyMenu.length ) {
           propertyMenu.push(<Divider key={i++}/>);
         }
         let {id, name} = group;
         propertyMenu.push(<MenuItem disabled value={id} key={id} primaryText={name}/>);
-        _each(group.properties, (property) => {
+        _each(filteredProps, (property) => {
           let {id, name} = property;
           propertyMenu.push(<MenuItem value={id} key={id} primaryText={name}/>);
         });
