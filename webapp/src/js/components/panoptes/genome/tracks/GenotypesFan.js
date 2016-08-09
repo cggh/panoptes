e9683bf8a7b3c653fb074014a684534aefb8ec62
Ben Jeffery <ben.jeffery@gmail.com>
2016-08-09 23:11:15 +0100

    Make fan more sparse on high column counts

--- webapp/src/js/components/panoptes/genome/tracks/GenotypesFan.js
+++ webapp/src/js/components/panoptes/genome/tracks/GenotypesFan.js
@@ -38,30 +38,37 @@ let GenotypesFan = React.createClass({
       return;
     }
     ctx.lineWidth = 1;//snp && snp.selected ? 2 : 1;
-    ctx.strokeStyle = 'rgba(80,80,80, 0.75)';
+    ctx.strokeStyle = 'rgba(80,80,80, 0.6)';
 
     const pixColWidth = colWidth * scale;
     const alpha = manualTween(pixColWidth, 5, 20);
     //Correspondence line
     const lineHeight = height - HAT_HEIGHT;
     const mid = lineHeight * 0.5;
+    let lastDrawn = null;
     for (let i = 0, iend = layoutBlocks.length; i < iend; ++i) {
       const [blockStart, blockEnd, colStart] = layoutBlocks[i];
       for (let j = blockStart, jCol = colStart+0.5; j < blockEnd; ++j, ++jCol) {
         // var key = col_primary_key[i];
-        const p = jCol * pixColWidth;
-        const o = (genomicPositions[j] - start) * scale;
+        const columnPixel = jCol * pixColWidth;
+        const genomicPixel = (genomicPositions[j] - start) * scale;
         // ctx.strokeStyle = 'rgba(80,80,80, 0.75)';
         // if (fncIsColSelected(key)) {
         //   ctx.strokeStyle = 'rgba(150, 0, 0, 0.75)';
         // }
-        ctx.beginPath();
-        ctx.moveTo(o, 0);
-        ctx.bezierCurveTo(o, mid, p, mid, p, lineHeight);
-        if (alpha === 0) {
-          ctx.lineTo(p, height);
+        if (pixColWidth > 2 ||    //Skip some if things are dense
+            !lastDrawn ||
+            genomicPixel - lastDrawn[0] > 2 ||
+            columnPixel - lastDrawn[1] > 2) {
+          ctx.beginPath();
+          ctx.moveTo(genomicPixel, 0);
+          ctx.bezierCurveTo(genomicPixel, mid, columnPixel, mid, columnPixel, lineHeight);
+          if (alpha === 0) {
+            ctx.lineTo(columnPixel, height);
+          }
+          ctx.stroke();
+          lastDrawn = [genomicPixel, columnPixel];
         }
-        ctx.stroke();
       }
     }
     if (alpha > 0) {
