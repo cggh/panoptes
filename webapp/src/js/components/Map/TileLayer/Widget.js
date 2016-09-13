799fccc1f74f958c1fb425b942a616f8317934f8
Lee Hart <lee.hart@well.ox.ac.uk>
2016-09-13 13:52:18 +0100

    Fix TileLayer not updating its attribution

--- webapp/src/js/components/Map/TileLayer/Widget.js
+++ webapp/src/js/components/Map/TileLayer/Widget.js
@@ -49,8 +49,22 @@ let TileLayerWidget = React.createClass({
 
     // FIXME: How to handle double quotes inside double quotes inside single quotes (!) in descriptions in templates.
 
+    // NB: Only the props url, opacity and zIndex are dynamic.
+    // The props attribution, maxZoom, minZoom will not update automatically.
+    /* https://github.com/PaulLeCam/react-leaflet/blob/master/docs/Components.md
+
+      The properties documented as dynamic properties are updated using the relevant Leaflet setter, other properties will not update the component when they are changed after the component is mounted.
+      All other properties are passed as the options argument to their corresponding Leaflet element and should work fine for static maps, it is however unlikely that they would updated if you change them afterwards.
+
+    */
+    // https://github.com/PaulLeCam/react-leaflet/blob/master/docs/Components.md#tilelayer
+
+    // We workaround this, to make sure that those props (e.g. attribution) are updated accordingly
+    // by changing the key whenever those props change, thereby causing React to remount the component.
+
     return (
       <TileLayer
+        key={JSON.stringify({attribution, maxZoom, minZoom})}
         attribution={attribution}
         children={undefined}
         maxZoom={maxZoom}
