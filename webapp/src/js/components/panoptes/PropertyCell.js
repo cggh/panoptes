8baa204a808f7b46e57a16ae4e3997c0e4cb8f60
Ben Jeffery <ben.jeffery@gmail.com>
2016-10-04 17:52:16 +0100

    Add valueDisplays and valueDescriptions

--- webapp/src/js/components/panoptes/PropertyCell.js
+++ webapp/src/js/components/panoptes/PropertyCell.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import HtmlToReact from 'html-to-react';
 import PureRenderMixin from 'mixins/PureRenderMixin';
 
 import Formatter from 'panoptes/Formatter';
 import Icon from 'ui/Icon';
 import FluxMixin from 'mixins/FluxMixin';
 import ItemLink from 'panoptes/ItemLink';
+import Tooltip from 'rc-tooltip';
+
+
+const htmlToReactParser = new HtmlToReact.Parser(React, {
+  lowerCaseAttributeNames: false,
+  lowerCaseTags: false,
+  recognizeSelfClosing: true
+});
+
 
 let PropertyCell = React.createClass({
   mixins: [
@@ -19,9 +29,19 @@ let PropertyCell = React.createClass({
 
   render() {
     let {prop, value} = this.props;
-    let text = Formatter(prop, value);
     let externalLinkIcon = <i className="fa fa-external-link external-link-icon"></i>;
+    let descriptionIcon = prop.valueDescriptions && prop.valueDescriptions[value] ?
+      <Tooltip placement="bottom"
+                 trigger={["click"]}
+                 overlay={htmlToReactParser.parse('<span>' + prop.valueDescriptions[value] + '</span>')}>
+          <Icon className="info" name="info-circle"/>
+        </Tooltip> :
+      null;
+
     if (prop.externalUrl) {
+      if (prop.valueDisplays) {
+        console.error(`Properties cannot have externalUrl and valueDisplays: ${prop.id}`)
+      }
       let refs = value.split(';');
       return (<span className="prop">
         {refs.map((ref, index) => (
@@ -34,6 +54,12 @@ let PropertyCell = React.createClass({
         </span>
         ))}
       </span>);
+    } else if (prop.valueDisplays && prop.valueDisplays[value]) {
+      //Wrap as can only have one tag passed to html-to-react
+      return <span className="prop">
+        {htmlToReactParser.parse('<span>' + prop.valueDisplays[value] + '</span>')}
+        {descriptionIcon}
+      </span>;
     } else if (prop.dispDataType == 'Boolean' && value !== '') {
       let val = (value === 1 || value === 'True');
       return <Icon className={(val ? 'prop bool true' : 'prop bool false')}
@@ -43,7 +69,8 @@ let PropertyCell = React.createClass({
       return <ItemLink table={prop.tableId} primKey={value} />;
     }
     return <span className="prop">
-      {text}
+      {Formatter(prop, value)}
+      {descriptionIcon}
     </span>;
   }
 
