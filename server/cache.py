e9a4fe9afeed1748306dd21326dda87cacb1b476
Ben Jeffery <ben.jeffery@gmail.com>
2016-10-12 10:06:30 +0100

    Missing file (again :( )

---
+++ server/cache.py
@@ -0,0 +1,12 @@
+import config
+from os.path import join
+from diskcache import FanoutCache
+
+with FanoutCache(join(config.BASEDIR, 'cache'),
+                 shards=4,
+                 size_limit=int(5e10),
+                 large_value_threshold=4096,
+                 eviction_policy='least-recently-used'
+                 ) as cache:
+    def getCache():
+        return cache
