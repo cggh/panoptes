285cbb36e14891a6914f6c14a991b217d86c8656
Ben Jeffery <ben.jeffery@gmail.com>
2017-01-26 15:41:24 +0000

    Reduce pie chart size

--- webapp/src/js/components/Map/TableMarkersLayer.js
+++ webapp/src/js/components/Map/TableMarkersLayer.js
@@ -511,7 +511,7 @@ let TableMarkersLayer = React.createClass({
       );
       let lengthRatio = this.lastLengthRatio || 1;
       if (pieAreaSum > 0) {
-        lengthRatio = Math.sqrt(0.3 / (pieAreaSum / pixelArea));
+        lengthRatio = Math.sqrt(0.15 / (pieAreaSum / pixelArea));
       }
       this.lastLengthRatio = lengthRatio;
       _forEach(clusterMarkers, (marker) => marker.radius = marker.originalRadius * lengthRatio);
