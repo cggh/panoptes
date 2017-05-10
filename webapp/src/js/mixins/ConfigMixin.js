2bbc6cc928b4eb50cfad9d5de30d52f8286940e6
Lee Hart <lee.hart@well.ox.ac.uk>
2017-05-10 16:11:47 +0100

    Add propertiesByColumn function to ConfigMixin

--- webapp/src/js/mixins/ConfigMixin.js
+++ webapp/src/js/mixins/ConfigMixin.js
@@ -12,6 +12,25 @@ let ConfigMixin = {
     flux.store('ConfigStore').on('change', this._setConfigFromFlux);
     this.tableConfig = () => this.config.tablesById[this.props.table];
     this._setConfigFromFlux();
+    this.propertiesByColumn = (column) => {
+      let tableId, columnId = undefined;
+      if (column.indexOf('.') !== -1) {
+        [tableId, columnId] = column.split('.');
+      } else {
+        tableId = this.props.table;
+        columnId = column;
+      }
+      if (this.config.tablesById[tableId] == undefined) {
+        console.error(`Table ${tableId} does not exist in this.config.tablesById.`);
+        return;
+      }
+      let properties = this.config.tablesById[tableId].propertiesById[columnId];
+      if (properties == undefined) {
+        console.error(`Properties not found for column ${columnId} in table ${tableId}.`);
+        return;
+      }
+      return properties;
+    };
 
   },
 
