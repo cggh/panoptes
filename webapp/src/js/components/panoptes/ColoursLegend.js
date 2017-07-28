ce06b17c8c436f56b8cbee0a0f44542fb960846b
Lee Hart <username@users.noreply.github.com>
2017-07-28 17:50:59 +0100

    Add "+N more" when maxLegendItems

--- webapp/src/js/components/panoptes/ColoursLegend.js
+++ webapp/src/js/components/panoptes/ColoursLegend.js
@@ -23,7 +23,7 @@ let ColoursLegend = React.createClass({
     return <div className="legend">
     {maxLegendItems !== undefined && legendElements.length < maxLegendItems ?
       legendElements
-    : legendElements.slice(0, maxLegendItems)
+    : legendElements.slice(0, maxLegendItems).concat([<div key="more" className="legend-element">+{legendElements.length - maxLegendItems} more</div>])
     }
     </div>;
   }
