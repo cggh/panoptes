8d0a399195345dd867012141d5b311a0bd0e65f6
Lee Hart <username@users.noreply.github.com>
2017-08-03 16:09:55 +0100

    Convert TableGeoJSONsLayer to use withAPIData

--- webapp/src/js/components/Map/TableGeoJSONsLayer.js
+++ webapp/src/js/components/Map/TableGeoJSONsLayer.js
@@ -1,33 +1,20 @@
 import React from 'react';
-
-// Mixins
-import ConfigMixin from 'mixins/ConfigMixin';
-import DataFetcherMixin from 'mixins/DataFetcherMixin';
 import FluxMixin from 'mixins/FluxMixin';
-
-// Lodash
 import _isEmpty from 'lodash/isEmpty';
-
-// Panoptes
-import API from 'panoptes/API';
-import ErrorReport from 'panoptes/ErrorReporter';
 import FeatureGroup from 'Map/FeatureGroup';
-import LRUCache from 'util/LRUCache';
 import SQL from 'panoptes/SQL';
 import {propertyColour} from 'util/Colours';
 import ColourPropertyLegend from 'panoptes/ColourPropertyLegend';
 import MapControlComponent from 'Map/MapControlComponent';
 import GeoJSON from 'GeoJSON';
-
+import withAPIData from 'hoc/withAPIData';
 
 const DEFAULT_GEOJSON_FILL_COLOUR = '#3d8bd5';
 
 let TableGeoJSONsLayer = React.createClass({
 
   mixins: [
-    FluxMixin,
-    ConfigMixin,
-    DataFetcherMixin('table', 'query', 'colourProperty', 'geoJsonProperty', 'labelProperty')
+    FluxMixin
   ],
 
   //NB: layerContainer and map might be provided as props rather than context (e.g. <Map><GetsProps><GetsContext /></GetsProps></Map>
@@ -45,7 +32,9 @@ let TableGeoJSONsLayer = React.createClass({
     table: React.PropTypes.string.isRequired,
     colourProperty: React.PropTypes.string,
     geoJsonProperty: React.PropTypes.string.isRequired,
-    labelProperty: React.PropTypes.string
+    labelProperty: React.PropTypes.string,
+    config: React.PropTypes.object, // This will be provided via withAPIData
+    data: React.PropTypes.array // This will be provided via withAPIData
   },
   childContextTypes: {
     layerContainer: React.PropTypes.object,
@@ -65,130 +54,60 @@ let TableGeoJSONsLayer = React.createClass({
     };
   },
 
-  getDefinedQuery(query, table) {
-    return (query || this.props.query) ||
-      ((table || this.props.table) ? this.config.tablesById[table || this.props.table].defaultQuery : null) ||
-      SQL.nullQuery;
+  getDefaultProps() {
+    return {
+      query: SQL.nullQuery,
+    };
   },
 
-  fetchData(props, requestContext) {
-
-    let {table, query, colourProperty, geoJsonProperty, labelProperty} = props;
-    let {changeLayerStatus} = this.context;
-
-    changeLayerStatus({loadStatus: 'loading'});
-
-    let tableConfig = this.config.tablesById[table];
-    if (tableConfig === undefined) {
-      console.error('tableConfig === undefined');
-      return null;
-    }
-
-    // Collect the set of columns to fetch.
-    let fetchColumns = new Set();
-    fetchColumns.add(tableConfig.primKey);
-
-    if (geoJsonProperty !== undefined && typeof geoJsonProperty === 'string' && geoJsonProperty !== '') {
-      if (tableConfig.propertiesById[geoJsonProperty] === undefined) {
-        console.error('The specified geoJsonProperty field ' + geoJsonProperty + ' was not found in the table ' + table);
-      } else {
-        fetchColumns.add(geoJsonProperty);
-      }
-    }
-
-    if (colourProperty !== undefined && typeof colourProperty === 'string' && colourProperty !== '') {
-      if (tableConfig.propertiesById[colourProperty] === undefined) {
-        console.error('The specified colourProperty field ' + colourProperty + ' was not found in the table ' + table);
-      } else {
-        fetchColumns.add(colourProperty);
-      }
-    }
-
-    if (labelProperty !== undefined && typeof labelProperty === 'string' && labelProperty !== '') {
-      if (tableConfig.propertiesById[labelProperty] === undefined) {
-        console.error('The specified labelProperty field ' + labelProperty + ' was not found in the table ' + table);
-      } else {
-        fetchColumns.add(labelProperty);
-      }
-    }
-
-    requestContext.request(
-      (componentCancellation) => {
-
-        // Get all GeoJSONs using the specified table.
-        let APIargs = {
-          columns: Array.from(fetchColumns),
-          database: this.config.dataset,
-          query: this.getDefinedQuery(query, table),
-          table: tableConfig.id,
-          transpose: true
-        };
-
-        return LRUCache.get(
-          'query' + JSON.stringify(APIargs), (cacheCancellation) =>
-            API.query({
-              cancellation: cacheCancellation,
-              ...APIargs
-            }),
-          componentCancellation
-        );
-
-      })
-      .then((data) => {
-
-        let geoJSONs = [];
-
-        // Translate the fetched data into GeoJSONs.
-
-        let tableConfig = this.config.tablesById[table];
-        let primKeyProperty = tableConfig.primKey;
-
-        for (let i = 0; i < data.length; i++) {
-
-          let primKey = data[i][primKeyProperty];
-
-          let valueAsColour = DEFAULT_GEOJSON_FILL_COLOUR;
-          let value = undefined;
-          if (colourProperty !== undefined && colourProperty !== null) {
-            let colourFunction = propertyColour(this.config.tablesById[table].propertiesById[colourProperty]);
-            let nullifiedValue = (data[i][colourProperty] === '' ? null : data[i][colourProperty]);
-            valueAsColour = colourFunction(nullifiedValue);
-            value = nullifiedValue;
-          }
-
-          let json = JSON.parse(data[i][geoJsonProperty]);
-
-          let geoJSON = {
-            table,
-            primKey,
-            title: labelProperty !== undefined ? labelProperty : primKey,
-            valueAsColour,
-            value,
-            json
-          };
-
-          geoJSONs.push(geoJSON);
-
-        }
-
-        this.setState({geoJSONs});
-        changeLayerStatus({loadStatus: 'loaded'});
-
-      })
-      .catch(API.filterAborted)
-      .catch(LRUCache.filterCancelled)
-      .catch((error) => {
-        ErrorReport(this.getFlux(), error.message, () => this.fetchData(props));
-        console.error(error);
-        changeLayerStatus({loadStatus: 'error'});
-      });
+  getDefinedQuery(query, table) {
+    return (query || this.props.query) ||
+      ((table || this.props.table) ? this.props.config.tablesById[table || this.props.table].defaultQuery : null) ||
+      SQL.nullQuery;
   },
 
   render() {
 
     let {layerContainer, map} = this.context;
-    let {colourProperty, table, labelProperty} = this.props;
-    let {geoJSONs} = this.state;
+    let {colourProperty, table, labelProperty, geoJsonProperty, config, data} = this.props;
+
+    if (data === undefined || data === null) {
+      return null;
+    }
+
+    // Translate the apiData data into GeoJSONs.
+    let geoJSONs = [];
+
+    let tableConfig = config.tablesById[table];
+    let primKeyProperty = tableConfig.primKey;
+
+    for (let i = 0; i < data.length; i++) {
+
+      let primKey = data[i][primKeyProperty];
+
+      let valueAsColour = DEFAULT_GEOJSON_FILL_COLOUR;
+      let value = undefined;
+      if (colourProperty !== undefined && colourProperty !== null) {
+        let colourFunction = propertyColour(config.tablesById[table].propertiesById[colourProperty]);
+        let nullifiedValue = (data[i][colourProperty] === '' ? null : data[i][colourProperty]);
+        valueAsColour = colourFunction(nullifiedValue);
+        value = nullifiedValue;
+      }
+
+      let json = JSON.parse(data[i][geoJsonProperty]);
+
+      let geoJSON = {
+        table,
+        primKey,
+        title: labelProperty !== undefined ? labelProperty : primKey,
+        valueAsColour,
+        value,
+        json
+      };
+
+      geoJSONs.push(geoJSON);
+
+    }
 
     if (_isEmpty(geoJSONs)) {
       return null;
@@ -230,6 +149,59 @@ let TableGeoJSONsLayer = React.createClass({
   }
 
 
+});
+
+
+TableGeoJSONsLayer = withAPIData(TableGeoJSONsLayer, ({config, props}) => {
+
+  let {table, query, colourProperty, geoJsonProperty, labelProperty} = props;
+
+  let tableConfig = config.tablesById[table];
+  if (tableConfig === undefined) {
+    console.error('tableConfig === undefined');
+    return null;
+  }
+
+  // Collect the set of columns to fetch.
+  let columns = new Set();
+  columns.add(tableConfig.primKey);
+
+  if (geoJsonProperty !== undefined && typeof geoJsonProperty === 'string' && geoJsonProperty !== '') {
+    if (tableConfig.propertiesById[geoJsonProperty] === undefined) {
+      console.error('The specified geoJsonProperty field ' + geoJsonProperty + ' was not found in the table ' + table);
+    } else {
+      columns.add(geoJsonProperty);
+    }
+  }
+
+  if (colourProperty !== undefined && typeof colourProperty === 'string' && colourProperty !== '') {
+    if (tableConfig.propertiesById[colourProperty] === undefined) {
+      console.error('The specified colourProperty field ' + colourProperty + ' was not found in the table ' + table);
+    } else {
+      columns.add(colourProperty);
+    }
+  }
+
+  if (labelProperty !== undefined && typeof labelProperty === 'string' && labelProperty !== '') {
+    if (tableConfig.propertiesById[labelProperty] === undefined) {
+      console.error('The specified labelProperty field ' + labelProperty + ' was not found in the table ' + table);
+    } else {
+      columns.add(labelProperty);
+    }
+  }
+
+  return {
+    data: {
+      method: 'query',
+      args: {
+        database: config.dataset,
+        table,
+        columns,
+        query,
+        transpose: true
+      }
+    }
+  };
 });
 
 export default TableGeoJSONsLayer;
