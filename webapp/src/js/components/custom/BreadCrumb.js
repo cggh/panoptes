609de54a6e7322699f80c77f16ae7c0fb332f880
Ben Jeffery <ben.jeffery@gmail.com>
2017-09-22 12:10:37 +0100

    Add obs custom components

---
+++ webapp/src/js/components/custom/BreadCrumb.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './page-template.scss'
+import KeyboardArrowRight from 'material-ui-icons/KeyboardArrowRight';
+import filterChildren from 'util/filterChildren';
+import DocLink from 'panoptes/DocLink';
+
+class BreadCrumb extends React.Component {
+  static displayName = 'BreadCrumb';
+
+  render() {
+    let {children, a, b} = this.props;
+    children = filterChildren(this, children);
+    children = children || [];
+    if (children.length === undefined) children = [children];
+
+    b === 'geography' ? children.unshift(<DocLink href="geography.html">Geography</DocLink>) :  null;
+    b === 'drugs' ? children.unshift(<DocLink href="drugs.html">Drugs</DocLink>) :  null;
+    b === 'genes' ? children.unshift(<DocLink href="genes.html">Genes</DocLink>) :  null;
+
+    a === 'pf' ? children.unshift(<DocLink href="pf.html">Plasmodium falciparum</DocLink>) :  null;
+    a === 'pv' ? children.unshift(<DocLink href="pv.html">Plasmodium vivax</DocLink>) :  null;
+    a === 'ag' ? children.unshift(<DocLink href="ag.html">Anopheles gambiae</DocLink>) :  null;
+
+    children.unshift(<span>Guidebook</span>);
+
+    return (
+      <div className="obs-breadcrumb">
+        {React.Children.map(children, (child, i) =>
+          [child, i < children.length - 1 ? <KeyboardArrowRight/>: null]
+        )}
+      </div>
+    );
+  }
+}
+
+export default BreadCrumb;
