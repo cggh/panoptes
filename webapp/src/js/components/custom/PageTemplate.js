8cee58ae19515894775a1f034d297176622c339a
Ben Jeffery <ben.jeffery@gmail.com>
2017-09-22 12:10:37 +0100

    Add obs custom components

---
+++ webapp/src/js/components/custom/PageTemplate.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './page-template.scss'
+
+class PageTemplate extends React.Component {
+  static displayName = 'PageTemplate';
+
+  render() {
+    let {children, bgurl} = this.props;
+    return (
+      <div className="obs-page-container">
+        <div className="obs-page-backdrop" style={{backgroundImage: `url(/panoptes/Docs/observatory/images/${bgurl})`}} />
+        <div className="horiz-centering-container vertical stack obs-page-content">
+              {children}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PageTemplate;
