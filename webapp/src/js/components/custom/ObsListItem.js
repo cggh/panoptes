5854f65c46e40004d0fb3e4b0a036298ea42356c
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2017-09-29 16:05:54 +0100

    Migrate obs compnents to mui-v1

--- webapp/src/js/components/custom/ObsListItem.js
+++ webapp/src/js/components/custom/ObsListItem.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import createReactClass from 'create-react-class';
-import filterChildren from 'util/filterChildren';
 import FluxMixin from 'mixins/FluxMixin';
-import {ListItem} from 'material-ui/List';
 import DocPage from 'panoptes/DocPage';
+import {ListItem} from 'material-ui/List';
 
 let ObsListItem = createReactClass({
   displayName: 'ObsListItem',
@@ -14,20 +13,20 @@ let ObsListItem = createReactClass({
 
   render() {
     let {children, table, primKey, href, ...other} = this.props;
-    children = filterChildren(this, children);
-    let [icon, title, link] = children;
     return (
-      <ListItem primaryText={title} leftIcon={icon} onClick={(e) => {
+      <ListItem button onClick={(e) => {
         e.stopPropagation(); //To prevent a popup containing this button bringing itself to the front
-        if (link) {
-          this.getFlux().actions.session.tabOpen(link);
-        }
-        else if (table && primKey) {
+        // if (link) {
+        //   this.getFlux().actions.session.tabOpen(link);
+        // } else
+        if (table && primKey) {
           this.getFlux().actions.panoptes.dataItemPopup({table, primKey: primKey});
         } else if (href) {
           this.getFlux().actions.session.tabOpen(<DocPage path={href}/>);
         }
-      }} {...other}/>
+      }} {...other}>
+        {children}
+      </ListItem>
     );
   }
 });
