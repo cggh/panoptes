65cb991e7c2965fbd91d6369533395f4f9bcb349
Ben Jeffery <ben.jeffery@gmail.com>
2017-12-01 14:36:33 +0000

    WIP

--- webapp/src/js/components/custom/ResistanceMap.js
+++ webapp/src/js/components/custom/ResistanceMap.js
@@ -5,15 +5,24 @@ import Card, {CardContent, CardHeader} from 'material-ui/Card';
 import Typography from 'material-ui/Typography'
 import filterChildren from 'util/filterChildren';
 import Map from 'components/Map/Map';
+import ItemTemplate from 'components/panoptes/ItemTemplate';
 import TileLayer from 'components/Map/TileLayer';
 import TableMarkersLayer from 'components/Map/TableMarkersLayer';
+import TableMarkersLayerCustomPopup from 'components/Map/TableMarkersLayerCustomPopup';
+import CustomPopup from 'components/Map/CustomPopup';
 import { FormLabel, FormControl, FormControlLabel, FormHelperText } from 'material-ui/Form';
 import Radio, { RadioGroup } from 'material-ui/Radio';
+import FluxMixin from 'mixins/FluxMixin';
+
 let ResistanceMap = createReactClass({
   displayName: 'ResistanceMap',
 
   propTypes: {
   },
+
+  mixins: [
+    FluxMixin,
+  ],
 
   getInitialState() {
     return {
@@ -59,7 +68,17 @@ let ResistanceMap = createReactClass({
                                            clusterMarkers={true}
                                            markerColourProperty={`${drug}resistant`}
                   /> :
-                  <TableMarkersLayer showLegend={true} table="sites" clusterMarkers={false}/>
+                  <TableMarkersLayerCustomPopup clickPrimaryKeyProperty="site_id" disableOnClickMarker={true} showLegend={true} table="sites" clusterMarkers={false}>
+                    <CustomPopup>
+                      <ItemTemplate flux={this.getFlux()} table="sites">
+                        {"{{name}} - {{#query 'country_id' table='pf_samples' distinct='true' query='{\"whcClass\":\"comparefixed\",\"isCompound\":false,\"ColName\":\"site_id\",\"CompValue\":\"{{site_id}}\",\"isRoot\":true,\"Tpe\":\"=\"}'}}\n" +
+                        "                        <QueryResult table=\"countries\" expression='\"name\"' query='{\"whcClass\":\"comparefixed\",\"isCompound\":false,\"ColName\":\"country_id\",\"CompValue\":\"{{country_id}}\",\"isRoot\":true,\"Tpe\":\"=\"}' />\n" +
+                        "                      {{/query}} <strong><QueryResult table=\"pf_samples\" query='{\"whcClass\":\"comparefixed\",\"isCompound\":false,\"ColName\":\"site_id\",\"CompValue\":\"{{site_id}}\",\"isRoot\":true,\"Tpe\":\"=\"}' /></strong> <em>P. Falciparum</em> samples from {{name}},\n" +
+                        "                        contributed by <strong><QueryResult table=\"pf_samples\" expression='[\"count\", [[\"distinct\", [\"study_id\"]]]]' query='{\"whcClass\":\"comparefixed\",\"isCompound\":false,\"ColName\":\"site_id\",\"CompValue\":\"{{site_id}}\",\"isRoot\":true,\"Tpe\":\"=\"}' /></strong> studies." +
+                        "<ItemLink table='sites' primKey='{{site_id}}'><Button color=\"primary\" raised label='more'></PopupButton></ItemLink>"}
+                      </ItemTemplate>
+                    </CustomPopup>
+                  </TableMarkersLayerCustomPopup>
                 }
 
               </Map>
