5c00c2f1a74f062840cc09f917125fd0ee9c0acb
Ben Jeffery <ben.jeffery@gmail.com>
2017-12-01 14:36:33 +0000

    WIP

--- webapp/src/js/util/ComponentRegistry.js
+++ webapp/src/js/util/ComponentRegistry.js
@@ -70,7 +70,6 @@ import {Tabs, Tab} from '@material-ui/core';
 typeByDisplayName['Tabs'] = Tabs;
 typeByDisplayName['Tab'] = Tab;
 
-
 console.info('Components: %o', typeByDisplayName);
 
 export default function(displayName) {
