164bba5df078fad8f95762ceee80841279d1d4af
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2017-12-14 15:48:38 +0000

    MUI Table description

--- webapp/src/js/components/panoptes/MuiDataTableView.js
+++ webapp/src/js/components/panoptes/MuiDataTableView.js
@@ -279,6 +279,7 @@ let MuiDataTableView = createReactClass({
               <TableRow>
                 {groupOrderedColumns.map((column, columnIndex) => {
                   let columnData = this.propertiesByColumn(column);
+                  console.log(columnData);
                   return (
                     <TableCell
                       key={'column_' + columnIndex}
