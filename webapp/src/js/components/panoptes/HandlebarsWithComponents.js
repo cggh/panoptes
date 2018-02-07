864304bb139580796735c3c801e0afeaed99409b
Lee Hart <4256466+leehart@users.noreply.github.com>
2018-02-07 16:35:10 +0000

    Add HandlebarsWithComponents component

---
+++ webapp/src/js/components/panoptes/HandlebarsWithComponents.js
@@ -0,0 +1,53 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import createReactClass from 'create-react-class';
+import HTMLWithComponents from 'panoptes/HTMLWithComponents';
+import customHandlebars from 'util/customHandlebars';
+
+// Mixins
+import ConfigMixin from 'mixins/ConfigMixin';
+import PureRenderMixin from 'mixins/PureRenderMixin';
+import FluxMixin from 'mixins/FluxMixin';
+
+let HandlebarsWithComponents = createReactClass({
+  displayName: 'HandlebarsWithComponents',
+
+  mixins: [
+    ConfigMixin,
+    PureRenderMixin,
+    FluxMixin,
+  ],
+
+  propTypes: {
+    children: PropTypes.oneOfType([PropTypes.string, PropTypes.array]).isRequired,
+  },
+
+  getInitialState() {
+    return {
+      rendered: ''
+    };
+  },
+
+  componentDidMount() {
+    let {children, ...other} = this.props;
+    this.handlebars = customHandlebars(this.config, ...other);
+    children = children instanceof Array ? children.join('') : children;
+    const result = this.handlebars.compile(children)({config: this.config, ...other});
+    if (result.then) {
+      result.then((rendered) =>
+        this.setState({
+          rendered
+        }));
+    } else {
+      this.setState({
+        rendered: result
+      });
+    }
+  },
+
+  render() {
+    return <HTMLWithComponents>{this.state.rendered}</HTMLWithComponents>;
+  },
+});
+
+export default HandlebarsWithComponents;
