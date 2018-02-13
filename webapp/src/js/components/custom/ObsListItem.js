533c3609fff88290afd2ce93022ab13096824080
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2018-02-13 14:19:38 +0000

    externalObsListItem

--- webapp/src/js/components/custom/ObsListItem.js
+++ webapp/src/js/components/custom/ObsListItem.js
@@ -25,24 +25,34 @@ let ObsListItem = createReactClass({
   render() {
     let {children, table, primKey, href, download, ...other} = this.props;
     if (!download) {
-      return (
-        <ListItem button onClick={(e) => {
-          e.stopPropagation(); //To prevent a popup containing this button bringing itself to the front
-          // if (link) {
-          //   this.getFlux().actions.session.tabOpen(link);
-          // } else
-          if (table && primKey) {
-            this.getFlux().actions.panoptes.dataItemPopup({table, primKey: primKey});
-          } else if (href) {
-            this.getFlux().actions.session.tabOpen(<DocPage path={href}/>);
-          }
-        }} {...other}>
-          {children}
-        </ListItem>
-      );
+      if (href && href.indexOf('://') === -1) {
+        //Internal link
+        return (
+          <ListItem button onClick={(e) => {
+            e.stopPropagation(); //To prevent a popup containing this button bringing itself to the front
+            // if (link) {
+            //   this.getFlux().actions.session.tabOpen(link);
+            // } else
+            if (table && primKey) {
+              this.getFlux().actions.panoptes.dataItemPopup({table, primKey: primKey});
+            } else if (href) {
+              this.getFlux().actions.session.tabOpen(<DocPage path={href}/>);
+            }
+          }} {...other}>
+            {children}
+          </ListItem>
+        );
+      } else {
+        //External link
+        return (<a style={{textDecoration:'none'}} href={href}>
+          <ListItem button {...other}>
+            {children}
+          </ListItem>
+        </a>);
+      }
     } else {
       return (
-        <a style={{textDecoration:'none'}} href={`/panoptes/Docs/${this.config.dataset}/${href}`}>
+        <a style={{textDecoration:'none'}} href={`${process.env.DATASET_URL_PATH_PREFIX}Docs/${this.config.dataset}/${href}`}>
           <ListItem button {...other}>
             {children}
           </ListItem>
