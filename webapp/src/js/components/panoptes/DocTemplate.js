e79a3c6ab759aeeafa9986c7b4ff8fde64221533
Lee Hart <4256466+leehart@users.noreply.github.com>
2018-02-23 15:04:58 +0000

    Make DocTemplates editable

--- webapp/src/js/components/panoptes/DocTemplate.js
+++ webapp/src/js/components/panoptes/DocTemplate.js
@@ -2,9 +2,11 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import createReactClass from 'create-react-class';
 import withAPIData from 'hoc/withAPIData';
-import FluxMixin from 'mixins/FluxMixin'; // Need this?
+import FluxMixin from 'mixins/FluxMixin';
 import ConfigMixin from 'mixins/ConfigMixin'; // Need this?
 import HandlebarsWithComponents from 'panoptes/HandlebarsWithComponents';
+import IconButton from 'material-ui/IconButton';
+import EditDocPage from 'panoptes/EditDocPage';
 
 let DocTemplate = createReactClass({
   displayName: 'DocTemplate',
@@ -20,8 +22,20 @@ let DocTemplate = createReactClass({
   },
 
   render() {
-    const {template, ...otherProps} = this.props;
-    return template === undefined ? null : React.createElement(HandlebarsWithComponents, otherProps, template);
+    const {path, template, ...otherProps} = this.props;
+    return (
+      [
+        this.config.user.isManager ?
+          <IconButton
+            aria-label="Edit"
+            className="fa fa-edit"
+            onClick={() => this.getFlux().actions.session.modalOpen(<EditDocPage path={path}/>)}
+            style={{position: 'absolute', left: '0', right: '0', margin: 'auto'}}
+          />
+          : null,
+        template === undefined ? null : React.createElement(HandlebarsWithComponents, otherProps, template)
+      ]
+    );
   }
 });
 
