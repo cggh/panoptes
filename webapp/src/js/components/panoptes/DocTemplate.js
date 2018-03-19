1e52f757ee60b3471785aa5d1e29cdbca0fd48be
Lee Hart <4256466+leehart@users.noreply.github.com>
2018-03-19 11:28:47 +0000

    Set z-index for DocTemplate edit button

--- webapp/src/js/components/panoptes/DocTemplate.js
+++ webapp/src/js/components/panoptes/DocTemplate.js
@@ -23,6 +23,8 @@ let DocTemplate = createReactClass({
 
   render() {
     const {path, template, ...otherProps} = this.props;
+    // NOTE: z-index of the Edit modal is currently set to 9997.
+    const editButtonZIndex = 9996;
     return (
       [
         this.config.user.isManager ?
@@ -30,7 +32,7 @@ let DocTemplate = createReactClass({
             aria-label="Edit"
             className="fa fa-edit"
             onClick={() => this.getFlux().actions.session.modalOpen(<EditDocPage path={path}/>)}
-            style={{position: 'absolute', left: '0', right: '0', margin: 'auto'}}
+            style={{position: 'absolute', left: '0', right: '0', margin: 'auto', zIndex: editButtonZIndex}}
           />
           : null,
         template === undefined ? null : React.createElement(HandlebarsWithComponents, otherProps, template)
