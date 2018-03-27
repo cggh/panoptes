6515a454bf8d31013fbef9aeb4b31914d35aa849
Lee Hart <4256466+leehart@users.noreply.github.com>
2018-03-27 17:55:29 +0100

    Add getfeed service

---
+++ server/responders/getfeed.py
@@ -0,0 +1,31 @@
+# This program is free software licensed under the GNU Affero General Public License.
+# You can find a copy of this license in LICENSE in the top directory of the source code or at <http://opensource.org/licenses/AGPL-3.0>
+
+import urllib2
+import xmltodict
+import DQXbase64
+import json
+from cache import getCache
+
+def response(returndata):
+    url = returndata['url']
+    
+    cache = getCache()
+    cacheKey = json.dumps([url])
+    returndata['content'] = None
+    if returndata['cache']:
+        try:
+            returndata['content'] = cache[cacheKey]
+        except KeyError:
+            pass
+    
+    if returndata['content'] is None:
+        file = urllib2.urlopen(url)
+        data = file.read()
+        file.close()
+        data = xmltodict.parse(data)
+        returndata['content'] = DQXbase64.b64encode_var2(json.dumps(data))
+        if returndata['cache']:
+            cache[cacheKey] = returndata['content']
+    
+    return returndata
