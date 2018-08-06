b9c4f428379947c8ece1df1bc257a2b97f068d8a
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2018-08-06 13:30:02 +0100

    Cache config

--- server/responders/getconfig.py
+++ server/responders/getconfig.py
@@ -2,11 +2,25 @@
 # This program is free software licensed under the GNU Affero General Public License.
 # You can find a copy of this license in LICENSE in the top directory of the source code or at <http://opensource.org/licenses/AGPL-3.0>
 
+import os
 import DQXDbTools
-import authorization
+from cache import getCache
 from importer import configReadWrite
 
 def response(returndata):
     DQXDbTools.CredentialInformation(returndata).VerifyCanDo(DQXDbTools.DbOperationRead(returndata['dataset']))
-    returndata['config'] = configReadWrite.readJSONConfig(returndata['dataset'])
+
+    cache = getCache()
+    cacheKey = 'config' + returndata['dataset']
+
+    if not os.getenv('STAGING', '') and not os.getenv('DEVELOPMENT', ''):
+        try:
+            result = cache[cacheKey]
+        except KeyError:
+            result = configReadWrite.readJSONConfig(returndata['dataset'])
+            cache.set(cacheKey, result, expire=5*60)
+    else:
+        result = configReadWrite.readJSONConfig(returndata['dataset'])
+
+    returndata['config'] = result
     return returndata
