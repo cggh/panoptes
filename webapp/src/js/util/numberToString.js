a8c01f7d98bd9831056c7eb7585279ba131a0196
Lee Hart <4256466+leehart@users.noreply.github.com>
2018-09-21 11:52:21 +0100

    Amend comments

--- webapp/src/js/util/numberToString.js
+++ webapp/src/js/util/numberToString.js
@@ -3,10 +3,10 @@ export default function numberToString(number) {
   // "toString" strips off trailing zeros, e.g. "1.00000" becomes "1".
   // Without "toString", a large number will end up with commas in it, e.g. 100,000,000,000,000,000,000
   // With "toString", floats below 0.000001 end up as "1e-7", and numbers above 1000000000000000000000 end up like 1e+21)
-  // The inner parseFloat converts anything to either a float or a NaN. Compare with Number() https://stackoverflow.com/questions/12227594/which-is-better-numberx-or-parsefloatx
-  // The outer numberToString converts any scientific notation back into a number.
+  // The initial parseFloat converts anything to either a float or a NaN. Compare with Number() https://stackoverflow.com/questions/12227594/which-is-better-numberx-or-parsefloatx
+  // The rest of numberToString converts any scientific notation back into a number.
 
-  // Credit: https://stackoverflow.com/questions/1685680/how-to-avoid-scientific-notation-for-large-numbers-in-javascript/46545519#46545519
+  // Credit: https://stackoverflow.com/questions/1685680/how-to-avoid-scientific-notation-for-large-numbers-in-javascript
   // Limitations: numberToString(0.0000002) "0.00000019999999999999998" https://stackoverflow.com/questions/1458633/how-to-deal-with-floating-point-number-precision-in-javascript
 
   let num = parseFloat(number);
