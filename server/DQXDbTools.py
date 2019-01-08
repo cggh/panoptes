3e928ca6f80056341042da0e4ff694e5e5f0520d
Lee Hart <4256466+leehart@users.noreply.github.com>
2019-01-08 14:59:59 +0000

    Add support for ENDSWITH comparison operator

--- server/DQXDbTools.py
+++ server/DQXDbTools.py
@@ -325,7 +325,7 @@ class WhereClause:
 
     def _CreateSelectStatementSub_Comparison(self, statm):
         #TODO: check that statm['ColName'] corresponds to a valid column name in the table (to avoid SQL injection)
-        if not(statm['Tpe'] in ['=', '<>', '<', '>', '<=', '>=', '!=', 'LIKE', 'CONTAINS', 'CONTAINS_CASE_INSENSITIVE', 'NOTCONTAINS', 'NOT_CONTAINS_CASE_INSENSITIVE', 'STARTSWITH', 'ISPRESENT', 'ISABSENT', '=FIELD', '<>FIELD', '<FIELD', '>FIELD', 'between', 'ISEMPTYSTR', 'ISNOTEMPTYSTR', '_subset_', '_note_']):
+        if not(statm['Tpe'] in ['=', '<>', '<', '>', '<=', '>=', '!=', 'LIKE', 'CONTAINS', 'CONTAINS_CASE_INSENSITIVE', 'NOTCONTAINS', 'NOT_CONTAINS_CASE_INSENSITIVE', 'STARTSWITH', 'ENDSWITH', 'ISPRESENT', 'ISABSENT', '=FIELD', '<>FIELD', '<FIELD', '>FIELD', 'between', 'ISEMPTYSTR', 'ISNOTEMPTYSTR', '_subset_', '_note_']):
             raise Exception("Invalid comparison statement {0}".format(statm['Tpe']))
 
         processed = False
@@ -444,6 +444,9 @@ class WhereClause:
             if operatorstr == 'STARTSWITH':
                 operatorstr = 'LIKE'
                 decoval = '{0}%'.format(decoval)
+            if operatorstr == 'ENDSWITH':
+                operatorstr = 'LIKE'
+                decoval = '%{0}'.format(decoval)
             if operatorstr == 'CONTAINS_CASE_INSENSITIVE':
                 operatorstr = 'LIKE'
                 decoval = '%{0}%'.format(decoval)
