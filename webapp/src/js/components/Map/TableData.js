1618cd9db090b15634286d6062f286a2f8f7c4dc
Ben Jeffery <ben.jeffery@well.ox.ac.uk>
2019-06-14 12:56:34 +0100

    Allow TableData to return groups

--- webapp/src/js/components/Map/TableData.js
+++ webapp/src/js/components/Map/TableData.js
@@ -6,9 +6,11 @@ import SQL from 'panoptes/SQL';
 import withAPIData from 'hoc/withAPIData';
 import filterChildren from 'util/filterChildren';
 import resolveJoins from 'panoptes/resolveJoins';
-import {propertyColour} from 'util/Colours';
 import _map from 'lodash.map';
 import _keys from 'lodash.keys';
+import _values from 'lodash.values';
+import _pick from 'lodash.pick';
+import _includes from 'lodash.includes';
 
 let TableData = createReactClass({
   displayName: 'TableData',
@@ -20,6 +22,7 @@ let TableData = createReactClass({
   propTypes: {
     table: PropTypes.string.isRequired,
     groupBy: PropTypes.array, // FIXME: support PropTypes.arrayOf(PropTypes.string),
+    clientGroupBy: PropTypes.array, // FIXME: support PropTypes.arrayOf(PropTypes.string),
     joins: PropTypes.array, // FIXME: support PropTypes.arrayOf(PropTypes.object),
     query: PropTypes.string,
     start: PropTypes.number,
@@ -58,24 +61,23 @@ let TableData = createReactClass({
 TableData = withAPIData(TableData, ({config, props}) => {
   //Extract all the known props even if we don't need them as those left are columns
   let {
-    children, table, query, orderBy, groupBy, start, stop, distinct,
+    children, table, query, orderBy, groupBy, clientGroupBy, start, stop, distinct,
     randomSample, cache, joins, iterate, ...columns
   } = props;
 
-  columns = _map(columns, (expr, as) => ({expr, as}));
-
+  let requestColumns = _map(columns, (expr, as) => ({expr, as}));
   let groupBySet = new Set(groupBy);
 
   const resolvedArgs = resolveJoins({
     database: config.dataset,
     table,
-    columns: columns,
+    columns: requestColumns,
     query, orderBy,
     groupBy: Array.from(groupBySet),
     start, stop, distinct,
     randomSample, cache, joins,
     //We need individual rows as objects if we are interating so transpose here
-    transpose: iterate
+    transpose: iterate || clientGroupBy
   }, config);
   return {
     requests: {
@@ -83,6 +85,41 @@ TableData = withAPIData(TableData, ({config, props}) => {
         method: 'query',
         args: resolvedArgs,
       },
+    },
+    postProcess: ({data}) => {
+      if (!clientGroupBy)
+        return data;
+      let sets = {};
+      let setOrder = [];
+      let getKey = (row) => {
+        let a = _values(_pick(row, clientGroupBy));
+        return a.toString();
+      };
+      for (let i = 0; i < data.length; i++) {
+        let key = getKey(data[i]);
+        if (!sets[key]) {
+          sets[key] = [];
+          setOrder.push(key);
+        }
+        sets[key].push(data[i]);
+      }
+      let groups = _values(_pick(sets, setOrder));
+      if (iterate)
+        return groups;
+      let returnColumns = {};
+      _keys(columns).forEach((column) =>
+        returnColumns[column] = []
+      );
+      for (let i = 0; i < groups.length; i++) {
+        _keys(columns).forEach((column) => {
+          if (_includes(clientGroupBy, column))
+            returnColumns[column].push(groups[i][0][column]);
+          else {
+            returnColumns[column].push(_map(groups[i], (group) => group[column]));
+          }
+        });
+      }
+      return {data: returnColumns};
     }
   };
 });
